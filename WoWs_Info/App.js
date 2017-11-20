4b771fb6fdde144fe5fa0f9a2d1c55caa3d83e01
Yiheng <development.henryquan@gmail.com>
2017-11-20 11:03:53 +0800

    Dynamic theme
    It is possible to change theme colour dynamically

--- WoWs_Info/App.js
+++ WoWs_Info/App.js
@@ -1,57 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+      <View style={styles.viewStyle}>
+        <StatusBar barStyle='light-content' backgroundColor={Theme.bgColour}/>
+        <Text style={styles.textStyle}>
+          Hello World
         </Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
+const styles = {
+  viewStyle: {
+    backgroundColor: Theme.bgColour,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
+  textStyle: {
     textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    padding: 20,
+  }
+}
