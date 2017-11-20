06001d75f650741d1f0e3a3f8f9f338d4d09084f
Yiheng <development.henryquan@gmail.com>
2017-11-20 11:03:53 +0800

    Dynamic theme
    It is possible to change theme colour dynamically

--- WoWs_Info/index.js
+++ WoWs_Info/index.js
@@ -1,4 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import { Theme } from './src/colour/colour';
+
+// Setting up Theme
 
 AppRegistry.registerComponent('WoWs_Info', () => App);
---
+++ WoWs_Info/src/colour/colour.js
@@ -0,0 +1,17 @@
+export const WoWsInfo = {
+  red: '#',
+  green: '#',
+  blue: '#',
+  cyan: '#',
+  purple: '#',
+  brown: '#',
+  yellow: '#',
+  orange: '#',
+  black: '#',
+}
+
+export var Theme = {
+  textColour: '#',
+  tintColour: '#',
+  bgColour: 'black',
+}
