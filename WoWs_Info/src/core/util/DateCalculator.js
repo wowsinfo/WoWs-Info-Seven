19beffad913d6159e94de3074d1e49ea68661373
Yiheng <development.henryquan@gmail.com>
2017-11-27 15:08:14 +0800

    Solve run-ios issue and more stuff
    React native may crash suddenly... I hope there is a super stable IDE for react native just like Xcode and Android Studio.
    
    Retrieve perference for ios devices
    New folder util

---
+++ WoWs_Info/src/core/util/DateCalculator.js
@@ -0,0 +1,20 @@
+class DateCalculator {
+  static getCurrDate() {
+    return '';
+  }
+
+  static calDateDifference(dateBefore, dateAfter) {
+    
+  }
+
+  static isNewDay() {
+    // Update curr date
+    return true;
+  }
+
+  static shouldUpdateToken() {
+    return false;
+  }
+}
+
+export {DateCalculator}
