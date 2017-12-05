02c2f1e670bc8465b42cf2b494f1fc5ed689862c
Yiheng <development.henryquan@gmail.com>
2017-12-05 11:36:20 +0800

    global config
    There are 4 servers and a userInfo.
    It should be global and saved locally

--- WoWs_Info/src/core/util/index.js
+++ WoWs_Info/src/core/util/index.js
@@ -1,7 +1,6 @@
 export * from './DataStorage';
 export * from './GameVersion';
 export * from './Language';
-export * from './StorageManager';
 export * from './DateCalculator';
 export * from './PlayerConverter';
 export * from './ServerManager';
