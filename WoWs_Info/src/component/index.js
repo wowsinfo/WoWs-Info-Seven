8fbfa49ab6f7fb32ef32cc9ed0e7428675721bf9
Yiheng <development.henryquan@gmail.com>
2017-12-10 19:25:13 +0800

    react-localization issue
    It does not work quite well on android... I wonder why

--- WoWs_Info/src/component/index.js
+++ WoWs_Info/src/component/index.js
@@ -1,2 +1,11 @@
+// Common
 export * from './common/WoWsStatusBar';
+
+// Wiki Cell
+export * from './Wiki/Cell/WikiCell';
+
+// Wiki Detail
+
+// Other
 export * from './SearchButton/SearchButton';
+
