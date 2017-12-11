e77c2e60d3ba43617da875d5ab930206eefe86b0
Yiheng <development.henryquan@gmail.com>
2017-12-11 14:06:33 +0800

    Logo might be a bit overwhelming
    Sometimes, it is too big...

--- WoWs_Info/src/component/common/WoWsLoading.js
+++ WoWs_Info/src/component/common/WoWsLoading.js
@@ -1,30 +1,23 @@
 import React from 'react';
-import { Image, ActivityIndicator, View, StyleSheet, Dimensions } from 'react-native';
+import { ActivityIndicator, View, StyleSheet } from 'react-native';
 
 class WoWsLoading extends React.PureComponent {
   render() {
     return (
       <View style={viewStyle}>
-        <Image style={[imageStyle, {tintColor: global.themeColor}]} source={require('../../img/LogoWhite.png')}/>
         <ActivityIndicator color={global.themeColor} size='large'/>
       </View>
     )
   }
 }
 
-let imageWidth = Dimensions.get('window').width * 0.341333 * 2;
 const styles = StyleSheet.create({
-  imageStyle: {
-    width: imageWidth,
-    height: imageWidth,
-    paddingBottom: 4,
-  },
   viewStyle: {
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
   }
 })
-const { imageStyle, viewStyle } = styles;
+const { viewStyle } = styles;
 
 export {WoWsLoading};
