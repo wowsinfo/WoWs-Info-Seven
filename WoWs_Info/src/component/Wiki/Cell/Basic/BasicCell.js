982f6e9c10e3ec4c185f5f4562d5c297569b6c27
Yiheng <development.henryquan@gmail.com>
2017-12-12 19:37:31 +0800

    WIki ?
    ToDo: Map and Ship
    Before that I have to make sure apk could be generated and ios project works as well.

--- WoWs_Info/src/component/Wiki/Cell/Basic/BasicCell.js
+++ WoWs_Info/src/component/Wiki/Cell/Basic/BasicCell.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, TouchableWithoutFeedback } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import { styles } from './BasicCellStyles'; 
 
 class BasicCell extends React.PureComponent {
   render() {
     return (
       <View style={viewStyle}>
-        <Image source={{uri: this.props.icon, cache: 'default'}} style={imageStyle} />
+        <TouchableWithoutFeedback onPress={() => this.showDetail()}>
+          <Image source={{uri: this.props.icon, cache: 'force-cache'}} style={imageStyle} />
+        </TouchableWithoutFeedback>
       </View>
     )
+  }
+
+  showDetail() {
+    let data = this.props.data;
+    Actions.WikiDetailScreen({data: data, title: data.key});
   }
 }
 
