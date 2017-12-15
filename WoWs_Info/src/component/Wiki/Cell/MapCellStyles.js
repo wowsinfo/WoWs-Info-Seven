88357cdf33a6f50070363ce53595f93f001927ff
Yiheng <development.henryquan@gmail.com>
2017-12-15 11:16:46 +0800

    Map
    It might be useful during clan battles

--- WoWs_Info/src/component/Wiki/Cell/MapCellStyles.js
+++ WoWs_Info/src/component/Wiki/Cell/MapCellStyles.js
@@ -1,10 +1,15 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  cardStyle: {
+  viewStyle: {
     flex: 1,
+    margin: 4,
+    height: 50,
+    justifyContent: 'center',
   },
   textStyle: {
+    fontSize: 16,
+    fontWeight: 'bold',
     textAlign: 'center',
   }
 })
