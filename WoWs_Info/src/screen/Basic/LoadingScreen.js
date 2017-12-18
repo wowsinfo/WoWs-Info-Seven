06359d5043d0f963e2f998ab2da6096d2b6efc38
Yiheng <development.henryquan@gmail.com>
2017-12-18 14:48:52 +0800

    Theme for LoadingScreen
    It is more nature and looks great!

--- WoWs_Info/src/screen/Basic/LoadingScreen.js
+++ WoWs_Info/src/screen/Basic/LoadingScreen.js
@@ -5,7 +5,7 @@ import { WoWsStatusBar } from '../../component/';
 class LoadingScreen extends React.PureComponent {
   render() {
     return (
-      <View style={ViewStyle} backgroundColor={global.themeColour}>
+      <View style={ViewStyle} backgroundColor={this.props.colour}>
         <WoWsStatusBar style={ImageStyle}/>
         <Image source={require('../../img/LogoWhite.png')}/>
         <ActivityIndicator color='white' size='large'/>
