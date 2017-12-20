abaed733852b458b463b4dce4ae1ef3f61b1431f
Yiheng <development.henryquan@gmail.com>
2017-12-20 11:25:38 +0800

    Buggy ship filter
    Well... It is not quite useful yet...

--- WoWs_Info/src/component/Wiki/Cell/WarshipCell.js
+++ WoWs_Info/src/component/Wiki/Cell/WarshipCell.js
@@ -3,27 +3,43 @@ import { View, Text, Image } from 'react-native';
 import { styles } from './WarshipCellStyles';
 
 class WarshipCell extends React.PureComponent {
-  componentWillMount() {
-    this.data = this.props.data;
-    this.image = this.data.images.small;
+  constructor(props) {
+    super();
+    this.loadData(props.data);
+    this.state = {
+      image: this.image,
+      tierName: this.tierName,
+    }
+  }
+
+  componentDidUpdate() {
+    this.loadData(this.props.data);
+    this.setState({
+      image: this.image,
+      tierName: this.tierName,
+    })
+  }
+
+  loadData(data) {
+    this.image = data.images.small;
     let tierList = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
 
     // For Chinese user, replace Japanese ships' name
-    var name = tierList[this.data.tier - 1] + ' ';
-    if (this.data.nation == 'japan' && global.apiLanguage.includes('zh')) {
-      let alias = global.aliasJson[this.data.ship_id];
+    var name = tierList[data.tier - 1] + ' ';
+    if (data.nation == 'japan' && global.apiLanguage.includes('zh')) {
+      let alias = global.aliasJson[data.ship_id];
       // Might be null sometimes if kongzhong.net has not updated their name yet
       if (alias != null) {
         this.tierName = name + alias;
-      } else this.tierName = name + this.data.name;
-    } else this.tierName = name + this.data.name;
+      } else this.tierName = name + data.name;
+    } else this.tierName = name + data.name;
   }
 
   render() {
     return (
       <View style={viewStyle}>
-        <Image source={{uri: this.image, cache: 'default'}} style={imageStyle} resizeMode='contain'/>
-        <Text style={textStyle}>{this.tierName}</Text>
+        <Image source={{uri: this.state.image, cache: 'default'}} style={imageStyle} resizeMode='contain'/>
+        <Text style={textStyle}>{this.state.tierName}</Text>
       </View>
     )
   }
