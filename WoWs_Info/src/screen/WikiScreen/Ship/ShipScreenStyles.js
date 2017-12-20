42e2787d5d18e6065a9a796b4ddbca8d489d9500
Yiheng <development.henryquan@gmail.com>
2017-12-20 12:16:10 +0800

    Great ship filter
    It works great currently

---
+++ WoWs_Info/src/screen/WikiScreen/Ship/ShipScreenStyles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  buttonStyle: {
+    height: 30, 
+    width: 30, 
+    backgroundColor: 'transparent',
+  },
+  filterButtonStyle: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: 'white',
+    margin: 8,
+  },
+  dropdownStyle: {
+    height: 400,
+  },
+  dropdownTextStyle: {
+    fontSize: 16,
+    textAlign: 'center',
+  }
+})
