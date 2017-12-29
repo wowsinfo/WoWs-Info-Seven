c446a056274ea3d3d6944bb6fdff690cfb4d889e
Yiheng <development.henryquan@gmail.com>
2017-12-29 19:04:49 +0800

    Fix CV not showing up issue
    Well, I thought it is the value but it is the key

--- WoWs_Info/src/core/util/ShipManager.js
+++ WoWs_Info/src/core/util/ShipManager.js
@@ -1,8 +1,8 @@
 class ShipManager {
   static getImage(type) {
     let typeJson = global.shipTypeJson;
-    switch (type) {
-      case typeJson.AirCarrier: return require('../../img/Aircraft Carrier.png');
+    switch (typeJson[type]) {
+      case typeJson.AirCarrier: return require('../../img/AircraftCarrier.png');
       case typeJson.Battleship: return require('../../img/Battleship.png');
       case typeJson.Cruiser: return require('../../img/Cruiser.png');
       case typeJson.Destroyer: return require('../../img/Destroyer.png');
