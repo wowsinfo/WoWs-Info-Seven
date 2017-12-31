b8408f118c33e40900ae65c8fb84c885196e05b7
Yiheng <development.henryquan@gmail.com>
2017-12-31 17:48:12 +0800

    Release build
    Amazingly, Android has a better experience for generating app

--- WoWs_Info/ios/WoWs_Info/AppDelegate.m
+++ WoWs_Info/ios/WoWs_Info/AppDelegate.m
@@ -17,8 +17,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  // Release
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  //jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"WoWs_Info"
