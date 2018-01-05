1b333f227ac25fbb3be07d072bcca7fe2114ae7f
Yiheng <development.henryquan@gmail.com>
2018-01-05 17:16:25 +0800

    Test Flight 2
    IOS is having really serious issue... I wonder why... Especially iPhone X..

--- WoWs_Info/ios/WoWs_Info/AppDelegate.m
+++ WoWs_Info/ios/WoWs_Info/AppDelegate.m
@@ -18,8 +18,8 @@
 {
   NSURL *jsCodeLocation;
   // Release
-  //jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  //jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"WoWs_Info"
