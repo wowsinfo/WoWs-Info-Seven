460bcecd6e3cdc97f6635758e9e9ab554a94d96a
Yiheng <development.henryquan@gmail.com>
2018-01-23 22:29:51 +0900

    Switcher for Android
    An array with label, func need to be passed through SwitcherButton

--- WoWs_Info/src/component/PlayerSearch/SwitcherButton.js
+++ WoWs_Info/src/component/PlayerSearch/SwitcherButton.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { WoWsTouchable } from '../../component';
-import { Image, View, Platform, Alert } from 'react-native';
+import { Image, View, Platform, Alert, Picker } from 'react-native';
 import strings from '../../localization';
 import { ServerManager } from '../../core/';
 
 class SwitcherButton extends React.PureComponent {
   render() {
-    const { onPress, imageStyle } = this.props;
+    const { imageStyle, data } = this.props;
     if (Platform.OS == 'ios') {
       return (
         <View style={{margin: 8}}>
@@ -16,13 +16,29 @@ class SwitcherButton extends React.PureComponent {
         </View>
       )
     } else {
-
+      return (
+        <View style={{margin: 8}}>
+          <Picker style={{height: 30, width: 30}} selectedValue={global.server} onValueChange={(value, index) => data[value].func()}>
+            { this.renderPickerItem(data) }
+          </Picker>
+        </View>
+      )
     }
   }
 
+  // Android
+  renderPickerItem(data) {
+    return data.map((data, i) => {
+      return (
+        <Picker.Item key={i} label={data.label} value={i}/>
+      )
+    })
+  }
+
+  // IOS
   showAlert = () => {
     const { data } = this.props;
-    console.log(data);
+    // console.log(data);
     var funcList = [];
     for (var i = 0; i < data.length; i++) funcList.push({text: data[i].label, onPress: data[i].func});
     Alert.alert(strings.change_server, strings.curr_server + ServerManager.getCurrName(global.server), funcList);
