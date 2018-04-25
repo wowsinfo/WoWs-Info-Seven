5af0631609b7fb72536417663d3a58f1bccbdf9e
Yiheng <development.henryquan@gmail.com>
2018-04-25 11:54:32 +1000

    DataStorage
    Some serious bugs are fixed. DataManager should be good to go.

--- WoWsInfo/src/app/App.js
+++ WoWsInfo/src/app/App.js
@@ -4,18 +4,38 @@ import { registerScreens } from '../screen';
 import { Blue, Green, Red } from 'react-native-material-color';
 import { getTextColour, statusBarColour } from '../constant/colour';
 import { DataStorage } from '../core/';
-import { language } from '../constant/language';
+import language from '../constant/language';
 
 registerScreens();
-Navigation.startTabBasedApp({
-  tabs: [
-    {
-      label: language.news_tab_label, title: language.news_tab_title,
-      screen: 'info.News',
-      navigatorStyle: {
-        navBarTextColor: getTextColour(Red),
-        navBarBackgroundColor: Red,
-        statusBarTextColorScheme: statusBarColour(Red)
+DataStorage.DataValidation().then(() => {
+  Navigation.startTabBasedApp({
+    tabs: [
+      {
+        label: language.news_tab_label, title: language.news_tab_title,
+        screen: 'info.News',
+        navigatorStyle: {
+          navBarTextColor: getTextColour(Red),
+          navBarBackgroundColor: Red,
+          statusBarTextColorScheme: statusBarColour(Red)
+        }
+      },
+      {
+        label: language.search_tab_label, title: language.search_tab_title,
+        screen: 'info.Search',
+        navigatorStyle: {
+          navBarTextColor: getTextColour(Blue),
+          navBarBackgroundColor: Blue,
+          statusBarTextColorScheme: statusBarColour(Blue)
+        }
+      },
+      {
+        label: language.settings_tab_label, title: language.settings_tab_title,
+        screen: 'info.Settings',
+        navigatorStyle: {
+          navBarTextColor: getTextColour(Green),
+          navBarBackgroundColor: Green,
+          statusBarTextColorScheme: statusBarColour(Green)
+        }
       }
     ],
     tabsStyle: {
--- WoWsInfo/src/constant/language.js
+++ WoWsInfo/src/constant/language.js
@@ -1,14 +1,23 @@
 import LocalizedStrings from 'react-native-localization';
 
-export const language = new LocalizedStrings({
+export default language = new LocalizedStrings({
  en:{
-   // Tabs
-   news_tab_title: 'WoWs News',
-   news_tab_label: 'News',
-   search_tab_title: 'WoWs Info Ultimate',
-   search_tab_label: 'Search',
-   settings_tab_title: 'Settings',
-   settings_tab_label: 'Settings',
+    // Tabs
+    news_tab_title: 'WoWs News',
+    news_tab_label: 'News',
+    search_tab_title: 'WoWs Info Ultimate',
+    search_tab_label: 'Search',
+    settings_tab_title: 'Settings',
+    settings_tab_label: 'Settings',
+    // Server
+    server_russia: 'ru',
+    server_europe: 'eu',
+    server_north_america: 'com',
+    server_asia: 'asia',
+    // Day difference
+    day_today: 'Today',
+    day_yesterday: 'Yesterday',
+    day_ago: ' ago',
  },
  zh: {
    // Tabs
