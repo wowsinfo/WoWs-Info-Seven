5a25ceeb45b5c7a3883a844103492c9fc0948a7e
Yiheng <development.henryquan@gmail.com>
2018-04-28 12:11:12 +1000

    Website & Tool
    Need to add all links and browser

--- WoWsInfo/src/screen/News/Tool.js
+++ WoWsInfo/src/screen/News/Tool.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import language from '../../constant/language';
+import { TextCell } from '../../component';
+
+const data = [{title: language.tool_wrinfo}, {title: language.tool_assist}];
 
 export default Tool = () => {
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <ScrollView>
+      { renderSection(data) }
+    </ScrollView>
   )
+}
+
+renderSection = (data) => {
+  return data.map(function(info, index) {
+    return <TextCell key={index} title={info.title}/>
+  })
 }
--- WoWsInfo/src/screen/News/Website.js
+++ WoWsInfo/src/screen/News/Website.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
+import { TextCell } from '../../component';
+import language from '../../constant/language';
+import { Divider } from 'react-native-elements';
+import { getTextColour } from '../../constant/colour';
+
+const data = [[language.web_official_title, {title: language.web_official}, {title: language.web_premium_store}, {title: language.web_official_wiki}], 
+              [language.web_statistics_title, {title: language.web_number}, {title: language.web_today}], 
+              [language.web_information_title, {title: language.web_sea_group}, {title: language.web_daily_bounce}, {title: language.web_facebook}], 
+              [language.web_ranking_title, {title: language.web_ranking}]];
 
 export default Website = () => {
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <ScrollView>
+      { renderWebsite(data) }
+    </ScrollView>
   )
+}
+
+renderWebsite = (data) => {
+  return data.map(function(info, index) {    
+    return info.map(function(info, index) {
+      // console.log(info, index);
+      if (index == 0) {      
+        return (   
+          <Text key={info} style={{padding: 8, backgroundColor: getTextColour(theme[500]), color: theme[500], fontWeight: 'bold'}}>{info}</Text>
+      )} else return <TextCell key={info.title} title={info.title}/>
+    })
+  })
 }
