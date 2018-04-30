25dd7fd789ffd2e270fe1510b25dc330af02c6bc
Yiheng <development.henryquan@gmail.com>
2018-04-30 15:53:29 +1000

    Upgrade & Next Ships
    Next Ships needs more styling and it should be able to navigate to a new screen (pop and push).

--- WoWsInfo/src/screen/Wiki/ShipDetail.js
+++ WoWsInfo/src/screen/Wiki/ShipDetail.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
-import { WoWsProgress } from '../../component';
+import { Text, StyleSheet, Image, ScrollView, FlatList, Alert } from 'react-native';
+import { View } from 'react-native-animatable';
+import { WoWsProgress, WoWsTouchable } from '../../component';
 import language from '../../constant/language';
-import { Blue } from 'react-native-material-color';
+import { Blue, Orange, Grey } from 'react-native-material-color';
 import { ShipDetailedInfo } from '../../core';
+import { getTextColour } from '../../constant/colour';
 
 const Tier = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
 export default class ShipDetail extends Component {
@@ -22,50 +24,22 @@ export default class ShipDetail extends Component {
       // Ship description
       parsed.description = description;
       // Collect status from everywhere
-      parsed.status = this.getStatus(mobility, weaponry, concealment, armour);
-      this.setState({data: parsed, isReady: true});
+      this.setState({data: data, isReady: true});
     })
-  }
-
-  /**
-   * Get general status for warship
-   * @param {*} mobility 
-   * @param {*} weaponry 
-   * @param {*} concealment 
-   * @param {*} armour 
-   */
-  getStatus(mobility, weaponry, concealment, armour) {
-    const { anti_aircraft, aircraft, artillery, torpedoes } = weaponry;
-    return {aa: anti_aircraft, aircraft: aircraft, gun: artillery, torp: torpedoes, 
-      concealment: concealment.total, armour: armour.total, mobility: mobility.total};
   }
 
   render() {
     const { tier, name, ship_id, icon } = this.props.info;
-    const { mainViewStyle, basicViewStyle, imageStyle, titleTextStyle, descriptionStyle } = styles;
-    
+    const { basicViewStyle, imageStyle } = styles;    
     const { data, isReady } = this.state;
     const { status, description } = data;
-
-    var tintColor = theme[500] == '#ffffff' ? Blue : theme[500];
     if (isReady) { return (
-      <ScrollView>
-        { /* Ship icon, name, tier, nation and price */ }        
-        <View style={basicViewStyle}>
-          {data_saver ? null : <Image source={{uri: icon, cache: 'default'}} style={imageStyle} resizeMode='contain'/>}
-          <Text>{Tier[tier - 1] + ' ' + name}</Text>
-          <Text style={descriptionStyle}>{description}</Text>
-        </View>
-        { /* Status with many progress */ }
-        <View>
-          <Text style={titleTextStyle}>{language.detail_status_title}</Text>
-          <WoWsProgress color={tintColor} value={status.armour} title={language.detail_survivability}/>
-          <WoWsProgress color={tintColor} value={status.gun} title={language.detail_artillery}/>
-          <WoWsProgress color={tintColor} value={status.torp} title={language.detail_torpedoes}/>
-          <WoWsProgress color={tintColor} value={status.aa} title={language.detail_aa}/>
-          <WoWsProgress color={tintColor} value={status.mobility} title={language.detail_maneuverabilty}/>
-          <WoWsProgress color={tintColor} value={status.concealment} title={language.detail_concealment}/>
-          <WoWsProgress color={tintColor} value={status.aircraft} title={language.detail_aircraft}/>
+      <ScrollView showsVerticalScrollIndicator={false}>  
+        <View animation='fadeInRight' ref='mainView'>
+          { this.renderBasic() }
+          { this.getStatus() }
+          { this.renderUpgrade() }
+          { this.renderNextShip() }
         </View>
       </ScrollView>
     )} else { return (
@@ -75,6 +49,102 @@ export default class ShipDetail extends Component {
       </View>
     )}
   }
+
+  /**
+   * Render section title
+   * @param {*} text 
+   */
+  renderTitle(text) {
+    return (
+      <Text style={[styles.titleTextStyle, {backgroundColor: theme[500], color: getTextColour(theme[500])}]}>{text}</Text>      
+    )
+  }
+
+  /**
+   * Render basic information for warship
+   */
+  renderBasic() {
+    const { basicViewStyle, imageStyle, descriptionStyle, tierTextStyle, basicTextStyle } = styles;
+    const { tier, name, icon, nation, type } = this.props.info;
+    const { description, price_credit, price_gold, is_premium, is_special } = this.state.data; 
+    const { encyclopedia, ship_type } = global.data;
+    return (
+      <View style={basicViewStyle}>
+        {data_saver ? null : <Image source={{uri: icon, cache: 'default'}} style={imageStyle} resizeMode='contain'/>}
+        <Text style={tierTextStyle}>{Tier[tier - 1] + ' ' + name}</Text>
+        <Text style={basicTextStyle}>{encyclopedia.ship_nations[nation] + '\n' + ship_type[type]}</Text>
+        <Text style={[basicTextStyle, {color: is_premium ? Orange : Grey}]}>{is_premium ? price_gold : price_credit}</Text>  
+        <Text style={descriptionStyle}>{description}</Text>
+      </View>
+    )
+  }
+
+  /**
+   * Get general status for warship
+   */
+  getStatus() {  
+    const { mobility, weaponry, concealment, armour } = this.state.data.default_profile;
+    const { anti_aircraft, aircraft, artillery, torpedoes } = weaponry;
+    const tintColor = theme[500] == '#ffffff' ? Blue : theme[500];    
+    return (
+      <View>
+        { this.renderTitle(language.detail_status_title) }
+        <WoWsProgress color={tintColor} value={armour.total} title={language.detail_survivability}/>
+        <WoWsProgress color={tintColor} value={artillery} title={language.detail_artillery}/>
+        <WoWsProgress color={tintColor} value={torpedoes} title={language.detail_torpedoes}/>
+        <WoWsProgress color={tintColor} value={anti_aircraft} title={language.detail_aa}/>
+        <WoWsProgress color={tintColor} value={mobility.total} title={language.detail_maneuverabilty}/>
+        <WoWsProgress color={tintColor} value={concealment.total} title={language.detail_concealment}/>
+        <WoWsProgress color={tintColor} value={aircraft} title={language.detail_aircraft}/>
+      </View>
+    )
+  }
+
+  /**
+   * Render upgrade list
+   */
+  renderUpgrade() {
+    const { mod_slots, upgrades } = this.state.data;
+    upgradeKey = (value, index) => index;
+    return (
+      <View>
+        { this.renderTitle(language.detail_upgrade_title + ' (' + mod_slots + ')') }
+        <FlatList keyExtractor={upgradeKey} showsHorizontalScrollIndicator={false} horizontal data={upgrades} renderItem={({item}) => {
+          let curr = data.consumable[item];
+          return (
+          <WoWsTouchable onPress={() => Alert.alert(curr.name, '\n' + curr.text, [{text: String(curr.price_credit)}])}>
+            <View><Image style={{width: 64, height: 64}} source={{uri: curr.icon}}/></View>
+          </WoWsTouchable>
+        )}}/>
+      </View>
+    )
+  }
+
+  /**
+   * Render next ship
+   */
+  renderNextShip() {
+    const { next_ships } = this.state.data;
+    if (Object.keys(next_ships).length == 0) return null;
+    else {
+      // Get all ships from next_ships
+      var ships = []; for (key in next_ships) ships.push({key: key, exp: next_ships[key]});
+      return (
+        <View>
+          { this.renderTitle(language.detail_next_ship_title) }
+          { ships.map(function(value, index) {
+            let curr = data.warship[value.key];
+            return (
+              <View>
+                <Image source={{uri: curr.icon}} style={{width: 128, height: 128}} resizeMode='contain'/>
+                <Text>{curr.name}</Text>
+              </View>
+            )
+          })}
+        </View>
+      )
+    }
+  }
 }
 
 const styles = StyleSheet.create({
@@ -83,10 +153,17 @@ const styles = StyleSheet.create({
   },
   titleTextStyle: {
     fontSize: 32, textAlign: 'center',
-    fontWeight: '500', padding: 8
+    fontWeight: '500', margin: 4, height: 40
+  },
+  tierTextStyle: {
+    fontSize: 24, marginBottom: 4
+  },
+  basicTextStyle: {
+    textAlign: 'center',
   },
   descriptionStyle: {
-    padding: 4, fontWeight: '300', textAlign: 'center'
+    padding: 4, fontWeight: '300', textAlign: 'center', 
+    marginTop: 8, marginBottom: 8
   },
   basicViewStyle: {
     alignItems: 'center'
