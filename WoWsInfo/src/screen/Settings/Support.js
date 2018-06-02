0eefd59e68a6ed0e72a8c6e9eda16800a64b150c
Yiheng <development.henryquan@gmail.com>
2018-06-02 22:38:08 +1000

    Support development
    Email feedback
    Write review
    Star repository
    Support on Patreon
    Remove ads (IOS)

---
+++ WoWsInfo/src/screen/Settings/Support.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class Suport extends Component {
+  static navigatorStyle = {
+    tabBarHidden: true
+  }
+
+  render() {
+    return (
+      <View></View>
+    )
+  }
+}
