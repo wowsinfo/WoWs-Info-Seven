cdb278e31572c26314c61d8ab34e2f6c29a9bb50
Yiheng <development.henryquan@gmail.com>
2018-06-12 11:28:48 +1000

    Revert "RN 0.56"
    This reverts commit cbee67fa7335dac01f7b3022b6a26f523ac43a90.

--- WoWsInfo/src/component/common/WoWsTouchable.js
+++ WoWsInfo/src/component/common/WoWsTouchable.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { TouchableNativeFeedback, TouchableBounce, Platform, View } from 'react-native';
+import { TouchableNativeFeedback, TouchableOpacity, Platform, View } from 'react-native';
 
 class WoWsTouchable extends Component {
   render() {
     const { children, style, ...props  } = this.props;
-    const Touchable = (Platform.OS == 'android') ? TouchableNativeFeedback : TouchableBounce;    
+    const Touchable = (Platform.OS == 'android') ? TouchableNativeFeedback : TouchableOpacity;    
     return (
       <Touchable {...props}>
         <View style={style}>
