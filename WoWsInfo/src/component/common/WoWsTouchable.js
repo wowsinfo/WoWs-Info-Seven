d44dda8e0383c123a599fcb4ce9a9db4ca4a1d7d
Yiheng <development.henryquan@gmail.com>
2018-06-12 11:27:32 +1000

    RN 0.56
    Well, it still does not work and probably it is because babel 7.

--- WoWsInfo/src/component/common/WoWsTouchable.js
+++ WoWsInfo/src/component/common/WoWsTouchable.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { TouchableNativeFeedback, TouchableOpacity, Platform, View } from 'react-native';
+import { TouchableNativeFeedback, TouchableBounce, Platform, View } from 'react-native';
 
 class WoWsTouchable extends Component {
   render() {
     const { children, style, ...props  } = this.props;
-    const Touchable = (Platform.OS == 'android') ? TouchableNativeFeedback : TouchableOpacity;    
+    const Touchable = (Platform.OS == 'android') ? TouchableNativeFeedback : TouchableBounce;    
     return (
       <Touchable {...props}>
         <View style={style}>
