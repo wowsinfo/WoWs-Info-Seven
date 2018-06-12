24f9988875e98b2cbd2e78c728a3cb92d2649986
Yiheng <development.henryquan@gmail.com>
2018-06-12 12:54:13 +1000

    OpenSource
    Update the UI for both IOS and Android
    
    
    Former-commit-id: ed1f9a9cccd408ab3a1dcce10bb6241692c7a021

--- WoWsInfo/src/component/cell/NewsCell.js
+++ WoWsInfo/src/component/cell/NewsCell.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { View, Image, Text, StyleSheet, Linking } from 'react-native';
-import { WoWsTouchable } from '../../component';
-import { scale } from 'react-native-size-matters';
+import { WoWsBounce, WoWsTouchable } from '../../component';
 import ElevatedView from 'react-native-elevated-view';
 import { GREY } from 'react-native-material-color';
 
@@ -9,21 +8,22 @@ class NewsCell extends Component {
   render() {
     const { image, title, time, link } = this.props.data;
     const { mainViewStyle, imageViewStyle, textViewStyle, imageStyle, titleStyle, timeStyle } = styles;    
+    const Touchable = android ? WoWsTouchable : WoWsBounce;
     return (
       <ElevatedView elevation={2} style={{margin: 8}}>
-        <WoWsTouchable onPress={() => Linking.openURL(link)}>
+        <Touchable onPress={() => Linking.openURL(link)}>
           <View style={mainViewStyle}>
             { data_saver ? null : (
               <View style={imageViewStyle}>
                 <Image source={{uri: image}} style={imageStyle} resizeMode='contain'/>
               </View>
-              )}
+            )}
             <View style={textViewStyle}>
               <Text style={titleStyle}>{title}</Text>
               <Text style={timeStyle}>{time}</Text>
             </View>
           </View>
-        </WoWsTouchable>
+        </Touchable>
       </ElevatedView>
     );
   }
@@ -31,7 +31,7 @@ class NewsCell extends Component {
 
 const styles = StyleSheet.create({
   mainViewStyle: {
-    flex: 1, flexDirection: 'row',
+    flex: 1, flexDirection: 'row', 
   },
   imageViewStyle: {
     width: 120,
