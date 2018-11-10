f5bce97751a13e14fe2b9bf886dddd7c602230f4
Yiheng <development.henryquan@gmail.com>
2018-11-10 16:29:04 +1100

    a new start
    Former-commit-id: 7d0ece4b75447e02074421b6fc6d7c1c82d84ffc

--- WoWsInfo/android/app/src/main/java/com/yihengquan/wowsinfo/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.yihengquan.wowsinfo;
-
-import com.reactnativenavigation.controllers.SplashActivity;
-
-public class MainActivity extends SplashActivity {
-
-}
--- WoWsInfo/index.js
+++
@@ -1,2 +0,0 @@
-import './src/constant/global';
-import './src/app/App';
