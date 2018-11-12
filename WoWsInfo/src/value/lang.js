fc10573387b2bdd97c62bfcc73762c525cfc9122
Yiheng <development.henryquan@gmail.com>
2018-11-12 22:33:38 +1100

    add more strings and finish lists
    Lots of copy and paste

--- WoWsInfo/src/value/lang.js
+++ WoWsInfo/src/value/lang.js
@@ -2,7 +2,8 @@ import LocalizedStrings from 'react-native-localization';
 
 export default lang = new LocalizedStrings({
   en: {
-    // Menu section
+    /// Menu section
+    // Wiki
     wiki_section_title: 'Encyclopedia',
     wiki_achievement: 'Achievement',
     wiki_warships: 'Warships',
@@ -11,6 +12,30 @@ export default lang = new LocalizedStrings({
     wiki_skills: 'Commander Skills',
     wiki_maps: 'Maps',
     wiki_collections: 'Collections',
+    // Extra
+    extra_section_title: 'Extra',
+    // Websites
+    website_title: 'Websites',
+    website_official_site: 'World of Warships',
+    website_premium: 'Premium Shop',
+    website_global_wiki: 'Global Wiki',
+    website_sea_group: 'Sea Group',
+    website_daily_bounce: 'The Daily Bounce',
+    website_numbers: 'WoWS Stats & Numbers',
+    website_today: 'Warships.Today',
+    website_ranking: 'Player Ranking',
+    website_models: 'Warships Models',
+    // YouTubers
+    youtuber_title: 'YouTubers',
+    youtuber_official: 'WoWs Official',
+    youtuber_flambass: 'Flambass',
+    youtuber_notser: 'Notser',
+    youtuber_jingles: 'The Mighty Jingles',
+    youtuber_panzerknacker: 'Panzerknacker',
+    youtuber_flamu: 'Flamu',
+    youtuber_yuro: 'Yuro',
+    youtuber_iChaseGaming: 'iChaseGaming',
+    youtuber_NoZoupForYou: 'NoZoupForYou',
   },
   zh: {
 
