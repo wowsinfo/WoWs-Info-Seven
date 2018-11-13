c3095856760c09c0deb959da3b51cb6d26dd1c78
Yiheng <development.henryquan@gmail.com>
2018-11-13 22:03:08 +1100

    adjust arrow button colour
    Former-commit-id: c03f70e1437239bcf7a6a809d18fa3b0d2adbbca

--- WoWsInfo/src/component/common/BackButton.js
+++ WoWsInfo/src/component/common/BackButton.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { View,  StyleSheet } from 'react-native';
 import { isIphoneX } from 'react-native-device-detection';
-import { List } from 'react-native-paper';
+import { List, Colors } from 'react-native-paper';
 
 class BackButton extends Component {
   render() {
     const { container } = styles;
     return (
       <View style={container}>
-        <List.Icon style={{padding: 4}} icon='arrow-back' />
+        <List.Icon color={Colors.grey500} icon='arrow-back' />
       </View>
     )
   }
