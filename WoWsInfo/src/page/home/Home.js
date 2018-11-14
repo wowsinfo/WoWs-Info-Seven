004cd53c47d76c26c55408abeb6ec8302e5ed741
Yiheng <development.henryquan@gmail.com>
2018-11-14 22:48:55 +1100

    create run time theme based on user preference
    Former-commit-id: a263c2cb6fc9c6589cd131f04ac937f5026f3073

--- WoWsInfo/src/page/home/Home.js
+++ WoWsInfo/src/page/home/Home.js
@@ -1,39 +1,74 @@
 import React, { Component } from 'react';
-import { StyleSheet, SafeAreaView, ScrollView } from 'react-native';
-import { withTheme, IconButton, Text, Colors, Divider, Surface, Card, Title, Paragraph } from 'react-native-paper';
+import { StyleSheet, SafeAreaView, ScrollView, StatusBar } from 'react-native';
+import { withTheme, IconButton, Text, Colors, Surface, 
+  Card, Title, Paragraph, DarkTheme, DefaultTheme } from 'react-native-paper';
 import { FloatingButton, LoadingModal } from '../../component';
 import { Actions } from 'react-native-router-flux';
-import { DataLoader } from '../../core';
+import { DataLoader, Guard } from '../../core';
+import { LOCAL } from '../../value/data';
+import { BLUE, GREY } from 'react-native-material-color';
 
 class Home extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      loading: true
+      loading: true,
+      dark: false
     }
 
     DataLoader.loadAll().then(data => {
       global.DATA = data;
-      props.theme.colors.accent = 'green';
-      this.setState({loading: false});
-      console.log(props);
+      const appTheme = Guard(DATA, `${LOCAL.theme}`, BLUE);
+      const darkMode = Guard(DATA, `${LOCAL.darkMode}`, true);
+
+      let customised = {};
+      // Decide whether dark mode is enabled
+      if (darkMode) {
+        customised = {
+          colors: {
+            ...DarkTheme.colors,
+            surface: Colors.grey900,
+            text: GREY[50],
+            primary: appTheme[500],
+            accent: appTheme[300],
+          }
+        };
+      } else {
+        customised = {
+          colors: {
+            ...DefaultTheme.colors,
+            primary: appTheme[500],
+            accent: appTheme[300],
+          }
+        };
+      }
+
+      console.log(customised);
+      props.theme.roundness = 8;
+      props.theme.dark = darkMode;
+      props.theme.colors = customised.colors;
+
+      this.setState({loading: false, dark: darkMode});
+      // console.log(props);
     })
   }
 
   render() {
     const { container, playerLabel, header, card } = styles;
-    const { loading } = this.state;
+    const { loading, dark } = this.state;
     // Show loading modal while getting all data from Asyncstorage
     if (loading) return <LoadingModal />
     return (
       <Surface style={container}>
+        <StatusBar barStyle={dark ? 'light-content' : 'dark-content'} 
+          backgroundColor={dark ? GREY[900] : GREY[200]}/>
         <SafeAreaView style={{height: '100%'}}>
           <Surface style={header}>
             <Text numberOfLines={1} style={playerLabel}>HenryQuan</Text>
             <IconButton icon='settings' size={24} color={Colors.grey500}
               onPress={() => Actions.Settings()}/>
           </Surface>
-          <Divider/>
           <ScrollView contentContainerStyle={{padding: 8}}>
             <Card style={card}>
               <Card.Content>
