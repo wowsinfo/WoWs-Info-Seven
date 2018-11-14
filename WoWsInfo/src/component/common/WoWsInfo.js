4be77e6ac78c5676944596503ce1f72a5e0b7e3d
Yiheng <development.henryquan@gmail.com>
2018-11-14 14:16:12 +1100

    add many symbols

--- WoWsInfo/src/component/common/WoWsInfo.js
+++ WoWsInfo/src/component/common/WoWsInfo.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
+import { isAndroid } from 'react-native-device-detection';
 import lang from '../../value/lang';
 
 class WoWsInfo extends Component {
   constructor(props) {
     super(props);
 
-    const name = [lang.wowsinfo, lang.wowsinfo_black, lang.wowsinfo_go, lang.wowsinfo_infinite,
-                 lang.wowsinfo_new, lang.wowsinfo_omega, lang.wowsinfo_phi, lang.wowsinfo_pro,
-                 lang.wowsinfo_ultimate, lang.wowsinfo_ultra, lang.wowsinfo_white, lang.wowsinfo_x, 
-                 lang.wowsinfo_y, lang.wowsinfo_z];
+    const name = [lang.wowsinfo, lang.wowsinfo_black, lang.wowsinfo_go, lang.wowsinfo_new, 
+                 lang.wowsinfo_pro, lang.wowsinfo_ultimate, lang.wowsinfo_ultra, lang.wowsinfo_white, 
+                 lang.wowsinfo_x, lang.wowsinfo_y, lang.wowsinfo_z, '>_<', '#', 
+                 '0_0', '^_^', '★', 'α', 'θ', 'Ω', 'Ф', '∞', '░', '( ͡° ͜ʖ ͡°)', '¯\_(ツ)_/¯'];
     // Get a name from used names
     this.lucky = name[Math.floor(Math.random() * name.length)];
   }
@@ -18,7 +20,7 @@ class WoWsInfo extends Component {
     const { container, title } = styles;
     return (
       <SafeAreaView style={container}>
-        <Text style={title}>{`WoWs Info ${this.lucky}`}</Text>
+        <Button style={title}>{`WoWs Info ${this.lucky}`}</Button>
       </SafeAreaView>
     )
   };
@@ -30,9 +32,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   title: {
-    fontSize: 17, fontWeight: '300',
+    fontSize: 17, fontWeight: isAndroid ? 'bold' : '300',
     textAlign: 'center',
-    marginBottom: 16
+    marginBottom: 8
   }
 });
 
