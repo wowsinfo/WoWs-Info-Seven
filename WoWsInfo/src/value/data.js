0c1b62c0cfcd0ed18aa64c8d0367d49180b3e6cd
Yiheng <development.henryquan@gmail.com>
2018-11-14 13:14:48 +1100

    update api link, local data value and add string format
    The goal is to handle it better than previous versions
    
    
    Former-commit-id: 8ee6f18c7f5c7481d7c7ed4dc30c7a392b804290

---
+++ WoWsInfo/src/value/data.js
@@ -0,0 +1,38 @@
+/*
+ * This is the format for pre 1.0.6 version
+ * The data conversion will last for a few months
+ */
+
+export const LocalData = {
+  has_ads: '@WoWs_Info:hasAds',
+  friend: '@WoWs_Info:playerList',
+  user_info: '@WoWs_Info:userInfo',
+  userdata: '@WoWs_Info:userData',
+  curr_version: '@WoWs_Info:currVersion',
+  game_version: '@WoWs_Info:gameVersion',
+  date: '@WoWs_Info:currDate',
+  // To do recent data for saved user only
+  last_update: '@WoWs_Info:lastUpdate',
+  data_saver: '@WoWs_Info:dataSaver',
+  server: '@WoWs_Info:currServer',
+  theme: '@WoWs_Info:themeColour',
+  first_launch: '@WoWs_Info:firstLaunch',
+  api_language: '@WoWs_Info:apiLanguage',
+  news_language: '@WoWs_Info:newsLanguage',
+  // IOS Tab
+  saved_tab: '@WoWs_Info:savedTab',
+}
+
+export const SavedData = {
+  language: '@Data:language',
+  encyclopedia: '@Data:encyclopedia',
+  ship_type: '@Data:ship_type',
+  achievement: '@Data:achievement',
+  commander_skill: '@Data:commander_skill',
+  collection: '@Data:collection',
+  collection_item: '@Data:collection_item',
+  warship: '@Data:warship',
+  map: '@Data:gameMap',
+  consumable: '@Data:consumable',
+  personal_rating: '@Data:personal_rating',
+}
