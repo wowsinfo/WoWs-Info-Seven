874685932de56e38021157b013fcd04b971e7818
Yiheng <development.henryquan@gmail.com>
2018-11-15 16:53:02 +1100

    center logo in about
    Former-commit-id: 1a49de4a307bebd0c27a05e47406144725bf9053

--- WoWsInfo/src/page/About.js
+++ WoWsInfo/src/page/About.js
@@ -1,45 +1,38 @@
 import React, { Component } from 'react';
-import { View, Image, ScrollView, StyleSheet, Dimensions } from 'react-native';
+import { Image, ScrollView, StyleSheet } from 'react-native';
 import { Surface, Text, Title } from 'react-native-paper';
 import { BackButton } from '../component';
+import { APP } from '../value/data';
 
 class About extends Component {
   constructor(props) {
     super(props);
-    
-    const { height } = Dimensions.get('window');
-    this.state = {
-      padding: height / 2 - 128
-    }
   }
 
   render() {
-    const { container, logo } = styles;
-    const { padding } = this.state;
+    const { container, scroll, logo } = styles;
     return (
-      <Surface style={container} onLayout={() => this.updatePadding()}>
-        <ScrollView contentContainerStyle={{paddingTop: padding, alignItems: 'center'}}>
+      <Surface style={container}>
+        <ScrollView contentContainerStyle={scroll}>
           <Image style={logo} source={require('../img/Logo.png')}/>
           <Title>WoWs Info</Title>
-          <Text>Sample Text</Text>
+          <Text>{APP.Version}</Text>
         </ScrollView>
         <BackButton />
       </Surface>
     )
   };
-
-  /**
-   * Update scrollview padding to make sure logo is about centered
-   */
-  updatePadding() {
-    const { height } = Dimensions.get('window');
-    this.setState({padding: height / 2 - 128});
-  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  scroll: {
+    flex: 1,
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   logo: {
     height: 128, width: 128
