885a6ee24335b1642414301016c1ff1c3331d86e
Yiheng <development.henryquan@gmail.com>
2018-11-15 16:40:29 +1100

    update about page and add some app info

--- WoWsInfo/src/page/About.js
+++ WoWsInfo/src/page/About.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Surface, Text } from 'react-native-paper';
+import { View, Image, ScrollView, StyleSheet, Dimensions } from 'react-native';
+import { Surface, Text, Title } from 'react-native-paper';
 import { BackButton } from '../component';
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+    
+    const { height } = Dimensions.get('window');
+    this.state = {
+      padding: height / 2 - 128
+    }
+  }
+
   render() {
-    const { container } = styles;
+    const { container, logo } = styles;
+    const { padding } = this.state;
     return (
-      <Surface style={container}>
-        <Text>About</Text>
+      <Surface style={container} onLayout={() => this.updatePadding()}>
+        <ScrollView contentContainerStyle={{paddingTop: padding, alignItems: 'center'}}>
+          <Image style={logo} source={require('../img/Logo.png')}/>
+          <Title>WoWs Info</Title>
+          <Text>Sample Text</Text>
+        </ScrollView>
         <BackButton />
       </Surface>
     )
   };
+
+  /**
+   * Update scrollview padding to make sure logo is about centered
+   */
+  updatePadding() {
+    const { height } = Dimensions.get('window');
+    this.setState({padding: height / 2 - 128});
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+  },
+  logo: {
+    height: 128, width: 128
   }
 });
 
