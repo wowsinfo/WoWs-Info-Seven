8fb2548a867dfec1e853f5e81d690daf0c15425f
Yiheng <development.henryquan@gmail.com>
2018-11-17 19:00:47 +1100

    update friend page to show a basic list

--- WoWsInfo/src/page/home/Friend.js
+++ WoWsInfo/src/page/home/Friend.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { List, Text, Colors, IconButton } from 'react-native-paper';
+import { LOCAL } from '../../value/data';
 
 class Friend extends Component {
   render() {
     const { container } = styles;
     return (
       <View style={container}>
-        <Text>Friend</Text>
+        <FlatList data={DATA[LOCAL.friendList]} renderItem={({item}) => 
+          <List.Item title={item.name} onPress={() => this.navigate(item)}
+          right={() => <IconButton color={Colors.grey500} icon='close'
+            onPress={() => null}/> }/>
+        }/>
       </View>
     )
   };
+
+  /**
+   * Navigate to player page with necessary information
+   * @param {any} info 
+   */
+  navigate(info) {
+
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+
   }
 });
 
