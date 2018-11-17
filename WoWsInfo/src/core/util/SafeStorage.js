c413b1a33981e0ec839ee45c41090de79b17f624
Yiheng <development.henryquan@gmail.com>
2018-11-17 12:53:51 +1100

    update safe storage

--- WoWsInfo/src/core/util/SafeStorage.js
+++ WoWsInfo/src/core/util/SafeStorage.js
@@ -1,13 +1,35 @@
 /**
  * Get and set storage keys safely
  */
-class SafeStorage {
-  static async get(key, dvalue) {
+import { AsyncStorage } from 'react-native';
 
+class SafeStorage {
+  /**
+   * Get a key from async storage and return a default value if not defined
+   * It will also save the default value if it is null
+   * @param {string} key 
+   * @param {any} value 
+   */
+  static async get(key, value) {
+    const data = await AsyncStorage.getItem(key);
+    if (data) {
+      // Return parsed value
+      return JSON.parse(data);
+    } else {
+      // Save the default value
+      this.set(key, value);
+      return value;
+    }
   }
 
-  static async set(key, value) {
-
+  /**
+   * Set any values to a key
+   * @param {string} key 
+   * @param {any} value 
+   */
+  static set(key, value) {
+    // Stringify values to json format
+    AsyncStorage.setItem(key, JSON.stringify(value));
   }
 }
 
