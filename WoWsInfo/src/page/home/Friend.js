81e7cf2107ac687783f07587e0b5558ed0843f19
Yiheng <development.henryquan@gmail.com>
2018-11-17 13:36:11 +1100

    add RS and Friend
    Former-commit-id: f8013e8402bd2d3ece791577364cce72997739e8

---
+++ WoWsInfo/src/page/home/Friend.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class Friend extends Component {
+  render() {
+    const { container } = styles;
+    return (
+      <View style={container}>
+        <Text>Friend</Text>
+      </View>
+    )
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+
+export { Friend };
