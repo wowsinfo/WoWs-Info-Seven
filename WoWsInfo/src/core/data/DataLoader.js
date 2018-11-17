c4cdde6660c6b2433d649c27777c7126b8b83a28
Yiheng <development.henryquan@gmail.com>
2018-11-17 13:21:15 +1100

    remove ADs and update data loader

--- WoWsInfo/src/core/data/DataLoader.js
+++ WoWsInfo/src/core/data/DataLoader.js
@@ -1,8 +1,9 @@
 /**
  * Load all saved data to global
  */
-import { AsyncStorage } from 'react-native';
-import { LOCAL, SAVED } from '../../value/data';
+import { SafeStorage } from '../util/SafeStorage';
+import { LOCAL, SAVED, APP } from '../../value/data';
+import { BLUE } from 'react-native-material-color';
 
 class DataLoader {
   /**
@@ -10,25 +11,42 @@ class DataLoader {
    * Return an object with all data
    */
   static async loadAll() {
-    // Get all keys from LOCAL and SAVED and merge theme
-    const data = Object.assign(await this.loadObj(LOCAL), await this.loadObj(SAVED));
+    let data = {};
+    const { apiLanguage, appVersion, gameVersion, firstLaunch, friendList, userData, 
+      userInfo, userServer, lastUpdate, theme, darkMode, date } = LOCAL;
+    
+    // Manully setting up SAVED section (they are all different)
+    this.loadEntry(data, apiLanguage, 'en');
+    this.loadEntry(data, appVersion, APP.Version);
+    this.loadEntry(data, gameVersion, '0.7.10.2');
+    this.loadEntry(data, firstLaunch, true);
+    this.loadEntry(data, friendList, [{name: 'HenryQuan', id: '', server: 3}]);
+    this.loadEntry(data, userData, {});
+    this.loadEntry(data, userInfo, {name: '', id: '', server: ''});
+    this.loadEntry(data, userServer, 3);
+    this.loadEntry(data, lastUpdate, new Date().toDateString());
+    this.loadEntry(data, theme, BLUE);
+    this.loadEntry(data, darkMode, false);
+    this.loadEntry(data, date, new Date().toDateString());
+
+    // SAVED section is about the same
+    for (let key in SAVED) {
+      const curr = SAVED[key];
+      // Get it from storage
+      data[curr] = await SafeStorage.get(curr, {});
+    }
+
     return data;
   }
 
   /**
-   * Get all key values and load them to global
-   * @param {object} section 
+   * Load and setup entries
+   * @param {object} data 
+   * @param {string} key 
+   * @param {any} value 
    */
-  static async loadObj(section) {
-    let data = {};
-    // Load all keys from object and get their value from storage
-    for (let k in section) {
-      const curr = section[k];
-      // Get it from storage
-      let val = await AsyncStorage.getItem(curr);
-      data[curr] = JSON.parse(val);
-    }
-    return data;
+  static async loadEntry(data, key, value) {
+    data[key] = await SafeStorage.get(key, value);
   }
 }
 
