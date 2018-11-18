00f79268c00eaa41adde2cd7fd5479368076bdeb
Yiheng <development.henryquan@gmail.com>
2018-11-18 21:27:20 +0800

    add server string array and start doing Downloader
    Former-commit-id: d82fe9b1da57806de84ed6e9c9a4a74dba591f9a

--- WoWsInfo/src/core/downloader/Downloader.js
+++ WoWsInfo/src/core/downloader/Downloader.js
@@ -1,7 +1,24 @@
-
+import { WoWsAPI } from "../../value/api";
 
 class Downloader {
-  constructor() {
+  constructor(server) {
+    // Convert server index to string
+    this.server = server;
+  }
+  /**
+   * Update all data if there is a new version except for force mode
+   * @param {Boolean} force 
+   */
+  static async updateAll(force) {
+    // Get server version
+
+  }
+
+  /**
+   * Get game server version of WoWs
+   */
+  static async getVersion() {
+    WoWsAPI.GameVersion;
   }
 }
 
