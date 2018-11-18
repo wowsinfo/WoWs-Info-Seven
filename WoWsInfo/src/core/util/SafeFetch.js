0063613f67c383fe81aed1bf32ef88d86234062f
Yiheng <development.henryquan@gmail.com>
2018-11-18 22:06:02 +0800

    safe fetch will make sure status code and api status is all good
    Former-commit-id: da880dc090f38d9b1b113995995c0249b38baba6

--- WoWsInfo/src/core/util/SafeFetch.js
+++ WoWsInfo/src/core/util/SafeFetch.js
@@ -1,13 +1,15 @@
-class SafeFetch() {
-  static get(api, server, extra) {
+class SafeFetch {
+  static get(api, server, extra='') {
     const format = require('string-format');
     let link = format(api, server) + extra;
-    console.log(link);
-    fetch(link).then(res => {
+    console.log(`SafeFetch\n${link}`);
+    return fetch(link).then(res => {
       // To make sure request is valid
       if (res.status === 200) return res.json();
       else return null;
-    })
+    }).then(json => {
+      if (json && json.status === 'ok') return json;
+    });
   }
 }
 
