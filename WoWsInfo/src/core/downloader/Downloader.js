ec7328a3379a79af8c3b7fb0925a5c3dfe264131
Yiheng <development.henryquan@gmail.com>
2018-11-18 22:13:01 +0800

    check if update is needed inside downloader
    Former-commit-id: 65c1b1c63793e27b5e4897aafdf35c86d16ae65d

--- WoWsInfo/src/core/downloader/Downloader.js
+++ WoWsInfo/src/core/downloader/Downloader.js
@@ -1,6 +1,7 @@
 import { WoWsAPI } from "../../value/api";
-import { SERVER } from "../../value/data";
+import { SERVER, APP, LOCAL } from "../../value/data";
 import { SafeFetch } from '../';
+import { Guard } from "../util/SafeGuard";
 
 class Downloader {
   constructor(server) {
@@ -15,7 +16,13 @@ class Downloader {
   async updateAll(force=false) {
     // Get server version
     console.log('Downloader\nChecking for new version');
-    this.getVersion();
+    let gameVersion = await this.getVersion();
+    let currVersion = DATA[LOCAL.gameVersion];
+    console.log(`Current: ${currVersion}\nAPI: ${gameVersion}`);
+    if (gameVersion > currVersion || force) {
+      // Update all data
+      console.log('Downloader\nUpdating all data from API')
+    }
   }
 
   /**
@@ -23,10 +30,8 @@ class Downloader {
    */
   async getVersion() {
     let json = await SafeFetch.get(WoWsAPI.GameVersion, this.server);
-    if (json) {
-      // Json data is valid
-      console.log(json);
-    }
+    // Guard ensures that there is always a value returned
+    return Guard(json, 'data.game_version', APP.GameVersion);
   }
 }
 
