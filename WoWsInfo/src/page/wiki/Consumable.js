4cabcaaf2fe7800d94b6d62ad507f3fc75df643e
Yiheng <development.henryquan@gmail.com>
2018-11-25 17:26:32 +0800

    update consumable to display logo
    Former-commit-id: 12323dc18ea416963ff651121b2413e829db9e08

--- WoWsInfo/src/page/wiki/Consumable.js
+++ WoWsInfo/src/page/wiki/Consumable.js
@@ -6,19 +6,63 @@
  */
 
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, Image, StyleSheet } from 'react-native';
+import GridView from 'react-native-super-grid';
 import { Surface } from 'react-native-paper';
+import { BackButton, LoadingModal } from '../../component';
+import { SAVED } from '../../value/data';
 
 class Consumable extends Component {
+  constructor(props) {
+    super(props);
+
+    // Load data depending on 'upgrade' prop
+    let data = [];
+    let consumable = DATA[SAVED.consumable];
+    for (let key in consumable) {
+      let curr = consumable[key];
+      if (curr.type === 'Modernization') {
+        data.push(curr);
+      }
+    }
+
+    // Sort by price
+    data.sort((a, b) => {
+      if (a.price_gold === 0) {
+        return a.price_credit - b.price_credit;
+      } else {
+        return a.price_gold - b.price_gold;
+      }
+    })
+
+    this.state = {
+      data: data
+    }
+  }
+
   render() {
     const { container } = styles;
-    const { upgrade } = this.props;
+
     return (
       <Surface style={container}>
-        <Text>{upgrade ? 'Upgrade' : 'Flag'}</Text>
+        <SafeAreaView>
+          { this.renderGrid() }
+          <BackButton />
+        </SafeAreaView>
       </Surface>
     )
   };
+
+  renderGrid() {
+    const { data } = this.state;
+    if (!data) return <LoadingModal />;
+
+    return (
+      <GridView itemDimension={64} items={data} renderItem={item => {
+        return <Image source={{uri: item.image}} style={{height: 64, width: 64}} />
+      }}/>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
