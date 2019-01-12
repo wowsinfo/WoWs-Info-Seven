d17f81235e1af6400e045462753795fc98f244da
Yiheng <development.henryquan@gmail.com>
2019-01-12 15:57:16 +0800

    support tint colour update

--- WoWsInfo/src/value/colour.js
+++ WoWsInfo/src/value/colour.js
@@ -1,5 +1,7 @@
 import { GREY } from 'react-native-material-color';
+import { DarkTheme, DefaultTheme } from 'react-native-paper';
 import { LOCAL } from './data';
+import { SafeStorage } from '../core';
 
 export const ThemeBackColour = () => {
   return {backgroundColor: DARKMODE ? GREY[900] : GREY[100]};
@@ -9,6 +11,36 @@ export const ThemeColour = () => {
   return DARKMODE ? GREY[900] : GREY[100];
 }
 
+export const UpdateDarkMode = () => {
+  global.DARKMODE = !DARKMODE;
+  SafeStorage.set(LOCAL.darkMode, DARKMODE);
+}
+
 export const TintColour = () => {
   return DATA[LOCAL.theme];
+}
+
+export const UpdateTintColour = (tint) => {
+  DATA[LOCAL.theme] = tint;
+  SafeStorage.set(LOCAL.theme, tint);
+
+  // Setup global dark theme
+  global.DARK = {
+    colors: {
+      ...DarkTheme.colors,
+      surface: 'black',
+      text: GREY[50],
+      primary: tint[500],
+      accent: tint[500],
+    }
+  };
+
+  // Setup global light theme
+  global.LIGHT = {
+    colors: {
+      ...DefaultTheme.colors,
+      primary: tint[500],
+      accent: tint[500],
+    }
+  };
 }
