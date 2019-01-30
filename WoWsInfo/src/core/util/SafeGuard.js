55d8dd7e9102c0209e2181ea83565787a05b6df6
Yiheng <development.henryquan@gmail.com>
2019-01-30 12:52:37 +1100

    check for null (0 will fail otherwise)
    Former-commit-id: c3bab409d9482d7309cb310b3a3ad709a7aee191

--- WoWsInfo/src/core/util/SafeGuard.js
+++ WoWsInfo/src/core/util/SafeGuard.js
@@ -6,7 +6,7 @@
  */
 export const Guard = (obj, path, dval) => {
   // This is a just a simple check if obj is null or undefined
-  if (path === '' && obj) return obj;
+  if (path === '' && obj != null) return obj;
   // check if object is valid and path does not start with or end with '.'
   if (!path.startsWith('.') && !path.endsWith('.')) {
     // get path as an array and it must have at least 2 elements
@@ -15,7 +15,7 @@ export const Guard = (obj, path, dval) => {
       // o is the object (accumulator), and n is from path (current value)
       // o && o[n] -> to go further or just return default value
       // only asking for the object
-      return p.reduce((o, n) => (o && o[n]) ? o[n] : dval, obj);
+      return p.reduce((o, n) => (o != null && o[n] != null) ? o[n] : dval, obj);
     }
   } 
   return dval;
