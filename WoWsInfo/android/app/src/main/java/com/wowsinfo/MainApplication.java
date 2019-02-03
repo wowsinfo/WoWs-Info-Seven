c356ba7c97ad9ed288fb378c507dde2c7c0b308e
Yiheng <development.henryquan@gmail.com>
2019-02-03 12:07:47 +1100

    update to react native 0.58.3

--- WoWsInfo/android/app/src/main/java/com/wowsinfo/MainApplication.java
+++ WoWsInfo/android/app/src/main/java/com/wowsinfo/MainApplication.java
@@ -3,8 +3,8 @@ package com.wowsinfo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactNativeLocalizationPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new ReactNativeLocalizationPackage()
       );
     }
 
