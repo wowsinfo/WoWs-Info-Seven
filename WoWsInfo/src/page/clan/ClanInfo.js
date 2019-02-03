ee7fa726fd355d041f4bbe5567038ae39a44147b
Yiheng <development.henryquan@gmail.com>
2019-02-03 15:46:14 +1100

    add ClanInfo

---
+++ WoWsInfo/src/page/clan/ClanInfo.js
@@ -0,0 +1,30 @@
+/**
+ * ClanInfo.js
+ * 
+ * 
+ */
+
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { WoWsInfo } from '../../component';
+
+class ClanInfo extends Component {
+  render() {
+    const { container } = styles;
+    return (
+      <WoWsInfo>
+        
+      </WoWsInfo>
+    )
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+
+export { ClanInfo };
