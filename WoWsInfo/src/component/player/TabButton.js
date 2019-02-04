2ab3accf07d391e6eaf7bb3e3b8e117e8d84917b
Yiheng <development.henryquan@gmail.com>
2019-02-04 14:53:45 +1100

    use TabButton to display icons (how to make them fill??)
    Former-commit-id: 55897ba6e4d1ce97e8096a26a09cbf50cd3d4701

--- WoWsInfo/src/component/player/TabButton.js
+++ WoWsInfo/src/component/player/TabButton.js
@@ -6,23 +6,29 @@
 
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { IconButton, Caption } from 'react-native-paper';
+import { Touchable } from '../common/Touchable';
 
 class TabButton extends Component {
   render() {
-    const { container } = styles;
+    const { container, text } = styles;
+    const { icon, color, label, onPress } = this.props;
     return (
-      <View style={container}>
-        <Text>TabButton</Text>
-      </View>
+      <Touchable style={container} onPress={() => null}>
+        <IconButton icon={icon} color={color} size={24}/>
+        <Caption style={text}>{label}</Caption>
+      </Touchable>
     )
   };
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+  },
+  text: {
+    marginTop: -14,
   }
 });
 
