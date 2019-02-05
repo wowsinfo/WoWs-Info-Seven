b45f7b4918819f6663b9a9504ef126fd28c6fe08
Yiheng <development.henryquan@gmail.com>
2019-02-05 22:15:48 +1100

    unpack data from pvp (for ships)
    Former-commit-id: 9fd04d486fe0afea993af243c725d6df93c6a504

--- WoWsInfo/src/component/player/ShipStat.js
+++ WoWsInfo/src/component/player/ShipStat.js
@@ -10,6 +10,22 @@ import { View, Text, StyleSheet } from 'react-native';
 class ShipStat extends Component {
   render() {
     const { container } = styles;
+    const { data } = this.props;
+    if (!data) return null;
+
+    const { distance, last_battle_time, pvp } = data;
+    const { 
+      art_agro, max_total_agro, torpedo_agro,
+      battles, wins, survived_battles, survived_wins,
+      damage_dealt, damage_scouting, max_damage_dealt, max_damage_scouting,
+      frags, max_frags_battle,
+      planes_killed, max_planes_killed,
+      ships_spotted, max_ships_spotted,
+      xp, max_xp,
+      ramming
+    } = pvp;
+
+    // There are two modes, basic player or ship statistics
     return (
       <View style={container}>
         <Text>ShipStat</Text>
