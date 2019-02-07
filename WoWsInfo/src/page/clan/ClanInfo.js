719d4d7dd704a5dae403f44bc656fad98064c1b7
Yiheng <development.henryquan@gmail.com>
2019-02-07 20:37:15 +1100

    get data, add clan tag and linking to wows numbers
    Former-commit-id: e9b5d09705d5b68ebaf7e94d7d5cd9c67ee12bf5

--- WoWsInfo/src/page/clan/ClanInfo.js
+++ WoWsInfo/src/page/clan/ClanInfo.js
@@ -5,20 +5,41 @@
  */
 
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Linking, StyleSheet } from 'react-native';
 import { WoWsInfo } from '../../component';
+import { SafeFetch, Guard } from '../../core';
+import { WoWsAPI } from '../../value/api';
+import { getDomain, getPrefix } from '../../value/data';
+import { Title } from 'react-native-paper';
 
 class ClanInfo extends Component {
   constructor(props) {
     super(props);
-    console.log(props.info);
+
+    const { clan_id, tag, server } = props.info;
+    this.state = {
+      id: clan_id,
+      tag: tag,
+      info: false
+    };
+
+    this.domain = getDomain(server);
+    this.prefix = getPrefix(server);
+
+    SafeFetch.get(WoWsAPI.ClanInfo, this.domain, clan_id).then(data => {
+      let clanInfo = Guard(data, `data.${clan_id}`, null);
+      if (clanInfo != null) {
+        this.setState({info: clanInfo});
+      }
+    });
   }
 
   render() {
     const { container } = styles;
+    const { info, tag, id } = this.state;
     return (
-      <WoWsInfo>
-        
+      <WoWsInfo title={`- ${id} -`} onPress={() => Linking.openURL(`https://${this.prefix}.wows-numbers.com/clan/${id}, ${tag}/`)}>
+        <Title>{tag}</Title>
       </WoWsInfo>
     )
   };
