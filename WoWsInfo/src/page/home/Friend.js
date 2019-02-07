736c5c16e67bc142e8538a73917dbfdfc8a8a5e0
Yiheng <development.henryquan@gmail.com>
2019-02-07 14:01:36 +1100

    this is not efficient (use an object instead of array)
    Former-commit-id: b20884eabc712ba806783ff0c13e80f7532a50a2

--- WoWsInfo/src/page/home/Friend.js
+++ WoWsInfo/src/page/home/Friend.js
@@ -5,17 +5,30 @@ import { LOCAL } from '../../value/data';
 import { SafeAction } from '../../core';
 
 class Friend extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: DATA[LOCAL.friendList]
+    };
+  }
+
   render() {
     const { container } = styles;
+    const { list } = this.state;
     return (
       <View style={container}>
-        <FlatList data={DATA[LOCAL.friendList]} renderItem={({item}) => 
+        <FlatList data={list} renderItem={({item, index}) => 
           <List.Item key={item.nickname} title={item.nickname} onPress={() => this.navigate(item)}
-            right={() => <IconButton color={Colors.grey500} icon='close' onPress={() => null}/> }/>}
+            right={() => <IconButton color={Colors.grey500} icon='close' onPress={() => this.removeFriend(item, index)}/> }/>}
           keyExtractor={i => i.account_id} showsVerticalScrollIndicator={false}/>
       </View>
     )
   };
+
+  removeFriend(item, index) {
+    let str = LOCAL.friendList;
+    let friend = DATA[str];
+  }
 
   /**
    * Navigate to player page with necessary information
