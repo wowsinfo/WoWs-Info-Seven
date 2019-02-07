be405527ef19606b98e72e43d734a12601c77cfb
Yiheng <development.henryquan@gmail.com>
2019-02-07 20:24:53 +1100

    pass data to ClanInfo

--- WoWsInfo/src/page/clan/ClanInfo.js
+++ WoWsInfo/src/page/clan/ClanInfo.js
@@ -9,6 +9,11 @@ import { View, Text, StyleSheet } from 'react-native';
 import { WoWsInfo } from '../../component';
 
 class ClanInfo extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props.info);
+  }
+
   render() {
     const { container } = styles;
     return (
