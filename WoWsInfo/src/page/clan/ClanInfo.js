7ff7b1af49e5947029ec58db82e838089aba6863
Yiheng <development.henryquan@gmail.com>
2019-02-07 21:31:23 +1100

    make InfoLabel touchable and able to push to players
    Former-commit-id: 43204ed3531050ca542f55dbd9a27b0a3a9f71dc

--- WoWsInfo/src/page/clan/ClanInfo.js
+++ WoWsInfo/src/page/clan/ClanInfo.js
@@ -6,10 +6,10 @@
 
 import React, { Component } from 'react';
 import { View, Text, ScrollView, FlatList, Linking, StyleSheet } from 'react-native';
-import { WoWsInfo, LoadingIndicator, InfoLabel, SectionTitle } from '../../component';
-import { SafeFetch, Guard, humanTimeString } from '../../core';
+import { WoWsInfo, LoadingIndicator, InfoLabel, SectionTitle, Touchable } from '../../component';
+import { SafeFetch, Guard, humanTimeString, SafeAction } from '../../core';
 import { WoWsAPI } from '../../value/api';
-import { getDomain, getPrefix } from '../../value/data';
+import { getDomain, getPrefix, getCurrServer } from '../../value/data';
 import { Title, Subheading, Paragraph, List, Caption } from 'react-native-paper';
 import { TintColour } from '../../value/colour';
 import lang from '../../value/lang';
@@ -27,6 +27,7 @@ class ClanInfo extends Component {
       valid: clan_id != null
     };
 
+    this.server = server;
     this.domain = getDomain(server);
     this.prefix = getPrefix(server);
 
@@ -66,7 +67,7 @@ class ClanInfo extends Component {
       console.log(data);
       const { horizontal } = styles;
       const {
-        created_at, creator_name, leader_name,
+        created_at, creator_name, creator_id, leader_name, leader_id,
         description, name, members, members_count
       } = data;
 
@@ -81,15 +82,18 @@ class ClanInfo extends Component {
         <View>
           <Subheading style={{color: TintColour()[500], alignSelf: 'center'}}>{name}</Subheading>
           <InfoLabel title={lang.clan_created_date} info={humanTimeString(created_at)}/>
-          <View style={horizontal}>
-            <InfoLabel title={lang.clan_creator_name} info={creator_name}/>
-            <InfoLabel title={lang.clan_leader_name} info={leader_name}/>
+          <View style={[horizontal, {flex: 1, justifyContent: 'space-around'}]}>
+            <InfoLabel title={lang.clan_creator_name} info={creator_name}
+              onPress={() => this.pushToMaster(creator_name, creator_id)}/>
+            <InfoLabel title={lang.clan_leader_name} info={leader_name}
+              onPress={() => this.pushToMaster(leader_name, leader_id)}/>
           </View>
           <Paragraph style={{padding: 16}}>{description}</Paragraph>
           <SectionTitle style={{alignSelf: 'flex-start'}} title={`${lang.clan_member_title} - ${members_count}`}/>
           <FlatList data={memberInfo} renderItem={({item}) => {
             return (
               <List.Item title={item.account_name} description={humanTimeString(item.joined_at)}
+                onPress={() => this.pushToPlayer(item)}
                 right={() => <Caption style={{paddingRight: 8, alignSelf: 'center'}}>{item.account_id}</Caption>}/>
             )
           }} showsVerticalScrollIndicator={false} keyExtractor={d => d.account_id}/>
@@ -98,7 +102,17 @@ class ClanInfo extends Component {
     } else {
       return <LoadingIndicator />
     }
-    
+  }
+
+  pushToMaster(name, id) {
+    let item = {nickname: name, account_id: id, server: this.server};
+    SafeAction('Statistics', {info: item});
+  }
+
+  pushToPlayer(item) {
+    item.nickname = item.account_name;
+    item.server = this.server;
+    SafeAction('Statistics', {info: item});
   }
 }
 
