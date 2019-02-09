b9203bce375bd820c0d926cecfc9f5f1ec387866
Yiheng <development.henryquan@gmail.com>
2019-02-09 20:04:03 +1100

    user 0 as min pr and ap
    Former-commit-id: 21554edf0bcf1f4addec5308649f0af6e021ffdd

--- WoWsInfo/src/component/player/RatingButton.js
+++ WoWsInfo/src/component/player/RatingButton.js
@@ -11,7 +11,7 @@ import { getColour, getComment, SafeAction } from '../../core';
 class RatingButton extends Component {
   render() {
     const { rating } = this.props;
-    if (rating == null || rating === -1) return null;
+    if (rating == null || rating === 0) return null;
     return (
       <Button color={getColour(rating)} onPress={() => SafeAction('Rating')}>
         {getComment(rating)}
