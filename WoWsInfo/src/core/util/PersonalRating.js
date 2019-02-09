77a7ba81806725a34afc96c5ea32793cabda665a
Yiheng <development.henryquan@gmail.com>
2019-02-09 15:38:25 +1100

    update PersonalRating

--- WoWsInfo/src/core/util/PersonalRating.js
+++ WoWsInfo/src/core/util/PersonalRating.js
@@ -1,14 +1,20 @@
 import { roundTo } from "./Util";
-import { Guard } from "./SafeGuard";
+import { SafeValue } from "./SafeGuard";
+import { SAVED } from "../../value/data";
 
-/**
- * Get personal rating for a single ship
- * @param {*} ship 
- */
-export const getShipRating = (ship) => {
-  if (ship == null) return -1;
-  // Damage, WinRate and Frags
-  const { battles, damage_dealt, frags, wins } = ship;
+const getOverall = (id) => DATA[SAVED.pr][id];
+
+const calRating = (actualDmg, expectedDmg, actualWins, expectedWins, actualFrags, expectedFrags) => {
+  // From https://wows-numbers.com/personal/rating by Wiochi
+  const rDmg = actualDmg / expectedDmg;
+  const rWins = actualWins / expectedWins;
+  const rFrags = actualFrags / expectedFrags;
+
+  const nDmg = Math.max(0, (rDmg - 0.4) / (1 - 0.4));
+  const nFrags = Math.max(0, (rFrags - 0.1) / (1 - 0.1));
+  const nWins = Math.max(0, (rWins - 0.7) / (1 - 0.7));
+
+  return 700 * nDmg + 300 * nFrags + 150 * nWins;
 };
 
 /**
@@ -17,18 +23,42 @@ export const getShipRating = (ship) => {
  */
 export const getOverallRating = (ships) => {
   if (ships == null) return -1;
-  for (let ship of ships) {
-    let rating = getShipRating(ship.pvp);
-    ship.rating = rating;
-    const battle = Guard(ship, 'pvp.battles', 0);
-    ship.ap = getAP(rating, battle);
 
-    // Accumulate damage, winrate and frags
+  let actualDmg = 0, expectedDmg = 0, actualWins = 0, expectedWins = 0, actualFrags = 0, expectedFrags = 0;
+  for (let ship of ships) {
+    const pvp = SafeValue(ship.pvp, null);
+    if (pvp) {
+      const overall = getOverall(ship.ship_id);
+      if (overall == null) continue;
+
+      const { battles, damage_dealt, frags, wins } = pvp;
+      const { average_damage_dealt, average_frags, win_rate } = overall;
+      
+      if (battles == 0) continue;
+      let currAvgDmg = damage_dealt / battles;
+      let currWinrate = wins / battles * 100;
+      let currFrags = frags / battles;
+
+      // Accumulate data
+      actualDmg += currAvgDmg;
+      actualWins += currWinrate;
+      actualFrags += currFrags;
+      expectedDmg += average_damage_dealt;
+      expectedWins += win_rate;
+      expectedFrags += average_frags;
+
+      // Calculate rating and ap
+      let rating = calRating(currAvgDmg, average_damage_dealt, currWinrate, win_rate, currFrags, average_frags);
+      ship.rating = rating;
+      ship.ap = getAP(rating, battles);
+    }
   }
+
+  return calRating(actualDmg, expectedDmg, actualWins, expectedWins, actualFrags, expectedFrags);
 };
 
 export const getAP = (rating, battle) => {
-  if (battle == 0) return -1;
+  if (rating == -1 || battle == 0) return -1;
   else return roundTo(Math.log10(battle) * rating);
 }
 
