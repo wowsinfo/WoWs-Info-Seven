cd6b7f4531cd0b40e90939a4637213d3da593bfd
Yiheng <development.henryquan@gmail.com>
2019-02-09 15:11:32 +1100

    remove extra views

--- WoWsInfo/src/core/util/PersonalRating.js
+++ WoWsInfo/src/core/util/PersonalRating.js
@@ -1,11 +1,14 @@
 import { roundTo } from "./Util";
+import { Guard } from "./SafeGuard";
 
 /**
  * Get personal rating for a single ship
  * @param {*} ship 
  */
 export const getShipRating = (ship) => {
-
+  if (ship == null) return -1;
+  // Damage, WinRate and Frags
+  const { battles, damage_dealt, frags, wins } = ship;
 };
 
 /**
@@ -13,11 +16,19 @@ export const getShipRating = (ship) => {
  * @param {*} ships 
  */
 export const getOverallRating = (ships) => {
+  if (ships == null) return -1;
+  for (let ship of ships) {
+    let rating = getShipRating(ship.pvp);
+    ship.rating = rating;
+    const battle = Guard(ship, 'pvp.battles', 0);
+    ship.ap = getAP(rating, battle);
 
+    // Accumulate damage, winrate and frags
+  }
 };
 
 export const getAP = (rating, battle) => {
-  if (battle == 0) return 0;
+  if (battle == 0) return -1;
   else return roundTo(Math.log10(battle) * rating);
 }
 
