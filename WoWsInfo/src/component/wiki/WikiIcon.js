5f01055d25b8da1be60bc4f9cdf01b3aeeaeacc0
Yiheng <development.henryquan@gmail.com>
2019-02-14 10:00:40 +1100

    use a default image instead

--- WoWsInfo/src/component/wiki/WikiIcon.js
+++ WoWsInfo/src/component/wiki/WikiIcon.js
@@ -10,18 +10,9 @@ import { LOCAL } from '../../value/data';
 import { Touchable } from '../common/Touchable';
 import { LoadingIndicator } from '../common/LoadingIndicator';
 
-class WikiIcon extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loading: true
-    }
-  }
-  
+class WikiIcon extends Component {  
   render() {
-    const { container, newLabel, indicator } = styles;
-    const { loading } = this.state;
+    const { container, newLabel } = styles;
     const { item, scale, warship, selected, ...props } = this.props;
     let width = 80;
     if (scale) width *= scale;
@@ -32,9 +23,8 @@ class WikiIcon extends Component {
         <View style={container}>
           { item ? item.new ? <View style={[newLabel, {backgroundColor: theme[500]}]}/> : null : null }
           <Image source={{uri: item.image ? item.image : item.icon}} resizeMode='contain'
-            onLoadEnd={() => this.setState({loading: false})} 
+            defaultSource={require('../../img/Unknown.png')}
             style={{width: width, height: width / 1.7}} />
-          { loading ? <LoadingIndicator style={indicator}/> : null }
         </View>
       );
     } else {
@@ -42,9 +32,8 @@ class WikiIcon extends Component {
         <Touchable style={[container, selected ? {borderColor: theme[500]} : null]} {...props}>
           { item.new ? <View style={[newLabel, {backgroundColor: DATA[LOCAL.theme][500]}]}/> : null }
           <Image source={{uri: item.image ? item.image : item.icon}} resizeMode='contain'
-            onLoadEnd={() => this.setState({loading: false})} 
+            defaultSource={require('../../img/Unknown.png')}
             style={{height: width, width: width}} />
-          { loading ? <LoadingIndicator style={indicator}/> : null }
         </Touchable>
       )
     }
@@ -61,9 +50,6 @@ const styles = StyleSheet.create({
     position: 'absolute', zIndex: 1,
     borderRadius: 99, bottom: 0,
     height: 8, width: 8
-  },
-  indicator: {
-    position: 'absolute',
   }
 });
 
