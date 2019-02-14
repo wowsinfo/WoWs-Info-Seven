02d24c8ca5ef1cc572789654e9aaa9432f5efaec
Yiheng <development.henryquan@gmail.com>
2019-02-14 12:09:08 +1100

    fix performance issue (IconButton... Let's prevent using it too much)

--- WoWsInfo/src/page/player/PlayerShip.js
+++ WoWsInfo/src/page/player/PlayerShip.js
@@ -1,10 +1,9 @@
 import React, { PureComponent } from 'react';
 import { View, Image, StyleSheet } from 'react-native';
-import { WoWsInfo, WarshipCell, Touchable, RatingButton, FooterPlus } from '../../component';
+import { WoWsInfo, WarshipCell, Touchable, RatingButton, FooterPlus, SimpleRating } from '../../component';
 import { getOverallRating, roundTo, getComment, getColourList, getColour, SafeAction } from '../../core';
 import { FlatGrid } from 'react-native-super-grid';
 import { SAVED } from '../../value/data';
-import { Text, IconButton } from 'react-native-paper';
 import { lang } from '../../value/lang';
 
 class PlayerShip extends PureComponent {
@@ -29,7 +28,7 @@ class PlayerShip extends PureComponent {
     const { data, rating } = this.state;
     return (
       <WoWsInfo title={`${lang.wiki_warship_footer} - ${data.length}`}>
-        <FlatGrid itemDimension={160} items={data} renderItem={({item}) => this.renderShip(item)} 
+        <FlatGrid itemDimension={180} items={data} renderItem={({item}) => this.renderShip(item)} 
           showsVerticalScrollIndicator={false} fixed/>
         <FooterPlus>
           <RatingButton rating={rating}/>
@@ -39,50 +38,14 @@ class PlayerShip extends PureComponent {
   };
 
   renderShip(item) {
-    const { centerText, horizontal, centerView } = styles;
-
     let ship = DATA[SAVED.warship][item.ship_id];
-    const { battles, wins, damage_dealt } = item.pvp;
-
-    let nothing = false;
-    if (battles === 0) nothing = true;
     return (
       <Touchable key={item.ship_id} onPress={() => SafeAction('PlayerShipDetail', {data: item})}>
         <WarshipCell item={ship} scale={1.8}/>
-        <View style={horizontal}>
-          <View style={centerView}>
-            <IconButton size={24} icon={require('../../img/Battle.png')}/>
-            <Text style={centerText}>{nothing ? '0' : battles}</Text>
-          </View>
-          <View style={centerView}>
-            <IconButton size={24} icon={require('../../img/WinRate.png')}/>
-            <Text style={centerText}>{nothing ? '0.0%' : `${roundTo(wins / battles * 100, 1)}%`}</Text>
-          </View>
-          <View style={centerView}>
-            <IconButton size={24} icon={require('../../img/Damage.png')}/>
-            <Text style={centerText}>{nothing ? '0' : roundTo(damage_dealt / battles)}</Text>
-          </View>
-        </View>
-        <View style={{backgroundColor: getColour(item.rating), height: 12, borderRadius: 99}}/>
+        <SimpleRating info={item}/>
       </Touchable>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  horizontal: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginTop: -8
-  },
-  centerText: {
-    alignSelf: 'center',
-    marginTop: -8
-  },
-  centerView: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
 
 export { PlayerShip };
