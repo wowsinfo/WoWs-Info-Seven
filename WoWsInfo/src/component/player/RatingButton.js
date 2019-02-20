34bc4fade0e0ef8889efe2e52690f4d6e5b4e1c5
Yiheng <development.henryquan@gmail.com>
2019-02-20 14:58:42 +1100

    saimple rating support null value, rating button support number mode and working rs with minor issue
    Former-commit-id: cc1d5ab0c23d7ac6bc6130c5af9cfcbda3ade6fa

--- WoWsInfo/src/component/player/RatingButton.js
+++ WoWsInfo/src/component/player/RatingButton.js
@@ -10,13 +10,23 @@ import { getColour, getComment, SafeAction } from '../../core';
 
 class RatingButton extends Component {
   render() {
-    const { rating } = this.props;
-    if (rating == null || rating === 0) return null;
-    return (
-      <Button mode='contained' color={getColour(rating)} onPress={() => SafeAction('Rating')} theme={{roundness: 0}}>
-        {getComment(rating)}
-      </Button>
-    );
+    const { rating, number } = this.props;
+    let curr = rating;
+    if (rating == null || rating === 0) curr = 0;
+
+    if (number) {
+      return (
+        <Button mode='contained' color={getColour(rating)}>
+          {rating}
+        </Button>
+      );
+    } else {
+      return (
+        <Button mode='contained' color={getColour(rating)} onPress={() => SafeAction('Rating')} theme={{roundness: 0}}>
+          {getComment(rating)}
+        </Button>
+      );
+    }
   };
 }
 
