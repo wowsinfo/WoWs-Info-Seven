8e7882454824250dfdb50b89704217c69e7c6a11
Henry <HenryQuan@users.noreply.github.com>
2019-02-26 18:56:18 +1100

    fix scolling issue inside collection (flexGrow does not limit view height to 100%)

--- WoWsInfo/src/page/wiki/BasicDetail.js
+++ WoWsInfo/src/page/wiki/BasicDetail.js
@@ -88,7 +88,7 @@ class BasicDetail extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    height: '100%',
+    flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center'
   },
