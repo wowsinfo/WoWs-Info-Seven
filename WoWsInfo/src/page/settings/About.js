2412316201bda7a308f365929134188f624fa621
Yiheng (Mac) <development.henryquan@gmail.com>
2019-03-08 21:57:03 +1100

    enable about in Settings and add random animation inside about

--- WoWsInfo/src/page/settings/About.js
+++ WoWsInfo/src/page/settings/About.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 import { Image, StyleSheet, Dimensions, Linking } from 'react-native';
+import * as Anime from 'react-native-animatable';
 import { WoWsInfo, Touchable } from '../../component';
 import { TintColour } from '../../value/colour';
 import { lang } from '../../value/lang';
+import { getRandomAnimation } from '../../core';
 
 class About extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      animation: 'pulse'
+    };
+
+    setInterval(() => {
+      this.setState({animation: getRandomAnimation()});
+    }, 2000);
   }
 
   render() {
     const { touch } = styles;
+    const { animation } = this.state;
     const { width, height } = Dimensions.get('window');
     let imageWidth = width > height ? height * 0.5 : width * 0.5;
 
     return (
       <WoWsInfo>
         <Touchable style={touch} onPress={() => Linking.openURL(lang.abour_github_link)}>
-          <Image style={{tintColor: TintColour()[500], height: imageWidth, width: imageWidth }}
-            source={require('../../img/Logo.png')} />
+          <Anime.View animation={animation} iterationCount='infinite' easing='ease' useNativeDriver>
+            <Image style={{tintColor: TintColour()[500], height: imageWidth, width: imageWidth }}
+              source={require('../../img/Logo.png')} />
+          </Anime.View>
         </Touchable>
       </WoWsInfo>
     )
