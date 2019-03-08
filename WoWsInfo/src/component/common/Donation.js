9ed7d435171de61db655e0bf84c95e6d44bca5db
Yiheng <HenryQuan@users.noreply.github.com>
2019-03-09 10:48:51 +1100

    add item skus

--- WoWsInfo/src/component/common/Donation.js
+++ WoWsInfo/src/component/common/Donation.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import * as RNIap from 'react-native-iap';
+import { View, Text, Platform, StyleSheet } from 'react-native';
+
+// Now, we have 4 tiers ($1, $3, $5 and $10) for donations
+const itemSkus = Platform.select({
+  ios: [
+    'point_1000',
+  ],
+  android: [
+    'com.yihengquan.wowsinfo.support1',
+    'com.yihengquan.wowsinfo.support3',
+    'com.yihengquan.wowsinfo.support5',
+    'com.yihengquan.wowsinfo.support10',
+  ],
+});
 
 class Donation extends Component {
   render() {
     const { container } = styles;
+
+
     return (
       <View style={container}>
         <Text>Donation</Text>
