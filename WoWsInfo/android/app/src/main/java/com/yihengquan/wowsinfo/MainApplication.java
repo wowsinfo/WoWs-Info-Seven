f42eb89e34e7d6bae52e3376d8642f21a95fb8c4
Yiheng (NUC) <development.henryquan@gmail.com>
2019-04-04 11:18:26 +1100

    try adding ads (I need to wait for a few days before it starts showing up)

--- WoWsInfo/android/app/src/main/java/com/yihengquan/wowsinfo/MainApplication.java
+++ WoWsInfo/android/app/src/main/java/com/yihengquan/wowsinfo/MainApplication.java
@@ -3,6 +3,7 @@ package com.yihengquan.wowsinfo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.sbugert.rnadmob.RNAdMobPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.dooboolab.RNIap.RNIapPackage;
 import com.horcrux.svg.SvgPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAdMobPackage(),
             new AsyncStoragePackage(),
             new RNIapPackage(),
             new SvgPackage(),
