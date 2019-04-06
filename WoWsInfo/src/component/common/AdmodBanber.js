e2e4b05f5f6a8d54f2003722bf49f6cf021be771
Yiheng (NUC) <development.henryquan@gmail.com>
2019-04-06 18:19:33 +1100

    show animation and then hide ads

--- WoWsInfo/src/component/common/AdmodBanber.js
+++ WoWsInfo/src/component/common/AdmodBanber.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
 import { AdMobBanner } from 'react-native-admob';
+import * as Anime from 'react-native-animatable';
+import { TintColour } from '../../value/colour';
 
 class AdmodBanber extends Component {
   constructor(props) {
@@ -11,22 +12,16 @@ class AdmodBanber extends Component {
   }
 
   render() {
-    const { container } = styles;
     const { success } = this.state;
     if (!success) return null;
     return (
-      <View style={container}>
-        <AdMobBanner adSize='smartBannerPortrait' onAdFailedToLoad={() => this.setState({success: false})}
+      <Anime.View style={{height: 36, backgroundColor: TintColour()[500]}} ref='admob'>
+        <AdMobBanner adSize='smartBannerPortrait' onAdFailedToLoad={() => this.refs['admob'].bounceOut(600).then(() => this.setState({success: false}))}
           adUnitID='ca-app-pub-5048098651344514/4703363983' testDevices={[AdMobBanner.simulatorId, 'd04604ce-ac57-4655-90c5-4a420f30b3dd']} />
-      </View>
+      </Anime.View>
     )
   };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    height: 36
-  }
-});
 
 export { AdmodBanber };
