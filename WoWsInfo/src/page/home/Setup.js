1609a0c9c4c52d80265714931e7606a79a2d0845
Yiheng (NUC) <development.henryquan@gmail.com>
2019-04-20 09:40:06 +1000

    hide fab if languages cannot be loaded

--- WoWsInfo/src/page/home/Setup.js
+++ WoWsInfo/src/page/home/Setup.js
@@ -61,7 +61,7 @@ class Setup extends Component {
             }} keyExtractor={i => i} numColumns={2}/> }
           </List.Section>
         </ScrollView>
-        <FAB icon='done' style={fab} onPress={loading ? null : () => this.finishSetup()}/>
+        <FAB visible={!loading} icon='done' style={fab} onPress={loading ? null : () => this.finishSetup()}/>
       </WoWsInfo>
     )
   };
