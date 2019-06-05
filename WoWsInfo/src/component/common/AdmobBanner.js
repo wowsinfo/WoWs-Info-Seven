fc637525948dafdc2c562531e2569e8c87c0b972
Yiheng (NUC) <development.henryquan@gmail.com>
2019-06-05 21:37:54 +1000

    hide ads after clicked and hide all future ads
    Former-commit-id: a38e4cc8d1cf449e45d77f305e1488ebb7829eb4

--- WoWsInfo/src/component/common/AdmobBanner.js
+++ WoWsInfo/src/component/common/AdmobBanner.js
@@ -5,6 +5,7 @@ import * as Anime from 'react-native-animatable';
 import { List } from 'react-native-paper';
 import { lang } from '../../value/lang';
 import { SafeAction } from '../../core';
+import { LOCAL } from '../../value/data';
 
 class AdmobBanner extends Component {
   constructor(props) {
@@ -20,21 +21,22 @@ class AdmobBanner extends Component {
       <List.Item title={lang.extra_support_wowsinfo} description={lang.extra_support_wowsinfo_subtitle} onPress={() => SafeAction('SupportMe')}/>
     );
 
-    // Because of this??
-    let unitID = 'ca-app-pub-5048098651344514/9965938758';
-    if (Platform.OS == 'ios') unitID = 'ca-app-pub-5048098651344514/4703363983';
+    let unitID = 'ca-app-pub-3940256099942544/6300978111';
+    // let unitID = 'ca-app-pub-5048098651344514/9965938758';
+    // if (Platform.OS == 'ios') unitID = 'ca-app-pub-5048098651344514/4703363983';
 
     return (
       <Anime.View>
-        <AdMobBanner adSize='smartBannerPortrait' onAdFailedToLoad={() => this.setState({success: false})}
-          onAdOpened={this.hideAds} onAdLeftApplication={this.hideAds} onAdClosed={this.hideAds}
-          adUnitID={unitID} testDevices={[AdMobBanner.simulatorId, 'd04604ce-ac57-4655-90c5-4a420f30b3dd']} />
+        <AdMobBanner adSize='smartBannerPortrait' onAdFailedToLoad={this.hideAds}
+          onAdClosed={this.hideAds}
+          adUnitID={unitID} testDevices={[AdMobBanner.simulatorId]} />
       </Anime.View>
     )
   };
 
   hideAds = () => {
     this.setState({success: false});
+    DATA[LOCAL.showBanner] = false;
   }
 }
 
