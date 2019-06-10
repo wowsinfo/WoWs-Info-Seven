5cb6f7fc211d17148ae0030c3a93587b8ed23add
Yiheng (NUC) <development.henryquan@gmail.com>
2019-06-10 20:37:04 +1000

    remove banner

--- WoWsInfo/src/component/common/AdmobBanner.js
+++ WoWsInfo/src/component/common/AdmobBanner.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
-import { Platform } from 'react-native';
-import { AdMobBanner } from 'react-native-admob';
-import * as Anime from 'react-native-animatable';
 import { List } from 'react-native-paper';
 import { lang } from '../../value/lang';
 import { SafeAction } from '../../core';
-import { LOCAL } from '../../value/data';
 
 class AdmobBanner extends Component {
   constructor(props) {
@@ -16,29 +12,25 @@ class AdmobBanner extends Component {
   }
 
   render() {
-    const { success } = this.state;
-    if (!success) return (
+    // Ask users to support me (especially IOS users)
+    return (
       <List.Item title={lang.extra_support_wowsinfo} description={lang.extra_support_wowsinfo_subtitle} onPress={() => SafeAction('SupportMe')}/>
     );
 
     // Test ads
     // let unitID = 'ca-app-pub-3940256099942544/6300978111';
 
-    let unitID = 'ca-app-pub-5048098651344514/9965938758';
-    if (Platform.OS == 'ios') unitID = 'ca-app-pub-5048098651344514/4703363983';
+    // let unitID = 'ca-app-pub-5048098651344514/9965938758';
+    // if (Platform.OS == 'ios') unitID = 'ca-app-pub-5048098651344514/4703363983';
 
-    return (
-      <Anime.View>
-        <AdMobBanner adSize='smartBannerPortrait' onAdFailedToLoad={this.hideAds}
-          onAdClosed={this.hideAds} adUnitID={unitID} testDevices={[AdMobBanner.simulatorId]} />
-      </Anime.View>
-    )
+    //
+    // return (
+    //   <Anime.View>
+    //     <AdMobBanner adSize='smartBannerPortrait' onAdFailedToLoad={this.hideAds}
+    //       onAdClosed={this.hideAds} adUnitID={unitID} testDevices={[AdMobBanner.simulatorId]} />
+    //   </Anime.View>
+    // )
   };
-
-  hideAds = () => {
-    this.setState({success: false});
-    DATA[LOCAL.showBanner] = false;
-  }
 }
 
 export { AdmobBanner };
