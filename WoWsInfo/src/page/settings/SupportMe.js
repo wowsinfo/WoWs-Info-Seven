235d1ab51c348f4bf7593ec709e2e6638c20e877
Yiheng (NUC) <development.henryquan@gmail.com>
2019-06-23 14:36:32 +1000

    update support page
    Former-commit-id: 61f7f09e8b4a24f4a9fb8eedbb4e0e71cf7e8d33

--- WoWsInfo/src/page/settings/SupportMe.js
+++ WoWsInfo/src/page/settings/SupportMe.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import { StyleSheet, Linking } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { List, Checkbox } from 'react-native-paper';
 import { Donation, WoWsInfo } from '../../component';
 import { lang } from '../../value/lang';
-import { APP, LOCAL } from '../../value/data';
+import { LOCAL } from '../../value/data';
 import { SafeStorage } from '../../core';
 
 class SupportMe extends Component {
   constructor(props) {
     super(props);
-
-    this.support = [{t: lang.support_patreon, d: APP.Patreon},
-      {t: lang.support_paypal, d: APP.PayPal},
-      {t: lang.support_wechat, d: APP.WeChat}];
 
     this.state = {
       banner: DATA[LOCAL.showBanner],
@@ -26,10 +22,6 @@ class SupportMe extends Component {
       <WoWsInfo hideAds>
         <List.Section title={lang.extra_support_wowsinfo}>
           <Donation />
-          { this.support.map(item => { return (
-            <List.Item title={item.t} key={item.t}
-              onPress={() => Linking.openURL(item.d)}/>
-          )})}
         </List.Section>
         <List.Section title={lang.support_ads}>
           <List.Item title={lang.support_ads_banner} onPress={() => this.updateBanner(!banner)} 
