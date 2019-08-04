1045bd047658721655681a397e5e5355b05963a2
Yiheng (2011) <development.henryquan@gmail.com>
2019-08-04 15:21:13 +1000

    working again and remove donations

--- WoWsInfo/ios/WoWsInfo/AppDelegate.m
+++ WoWsInfo/ios/WoWsInfo/AppDelegate.m
@@ -17,8 +17,8 @@
 {
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                              moduleName:@"WoWsInfo"
-                                              initialProperties:nil];
+                                                   moduleName:@"WoWsInfo"
+                                            initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
@@ -32,11 +32,11 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  #if DEBUG
-    return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-  #else
-    return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-  #endif
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
 }
 
 @end
