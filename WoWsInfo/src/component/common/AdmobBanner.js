e2e6af232efe1fcb1f5108e03898fcf4edba761b
Yiheng (NUC) <development.henryquan@gmail.com>
2019-08-17 11:07:08 +1000

    update packages and update pro button

--- WoWsInfo/src/component/common/AdmobBanner.js
+++ WoWsInfo/src/component/common/AdmobBanner.js
@@ -1,30 +1,20 @@
 import React, { Component } from 'react';
 import { View, Platform } from 'react-native';
-import { List } from 'react-native-paper';
+import { List, Button } from 'react-native-paper';
 import { lang } from '../../value/lang';
 import { SafeAction } from '../../core';
 
 class AdmobBanner extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      success: true
-    }
   }
 
   render() {
-    // Ask users to support me (especially IOS users)
-    const { success } = this.state;
+    // Pro version
     return (
-      <List.Item title={lang.extra_support_wowsinfo} description={lang.extra_support_wowsinfo_subtitle} onPress={() => SafeAction('SupportMe')}/>
+      <Button mode='contained' icon='lock-open'>Unlock pro features</Button>
     );
   };
-
-  hideAds = () => this.setState({success: false});
-  logError = err => {
-    console.log('err', err);
-    this.setState({success: false});
-  }
 }
 
 export { AdmobBanner };
