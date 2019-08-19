e111f9e6cf2368c06db4cd516a87b5515a9bd499
Yiheng (NUC) <development.henryquan@gmail.com>
2019-08-19 20:36:27 +1000

    working on android

--- new/wowsinfo/App.tsx
+++ new/wowsinfo/App.tsx
@@ -5,12 +5,26 @@
  * Generated with the TypeScript template
  * https://github.com/react-native-community/react-native-template-typescript
  *
+ * @format
  */
 
-import React from 'react';
-import { Fragment } from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
-import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   const usingHermes = typeof HermesInternal === 'object' && HermesInternal !== null;
--- new_wowsinfo/index.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Entry point of this app
- */
-
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as AppName }  from './app.json';
-
-AppRegistry.registerComponent(AppName, () => App);
