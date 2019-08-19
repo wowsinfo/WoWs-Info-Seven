e5c2ba1fd4ae95a4631bdee0c58177b8b8daa64c
Yiheng (Surface) <development.henryquan@gmail.com>
2019-08-19 17:15:39 +1000

    change index.js to index.tsx and allow importing json

--- new_wowsinfo/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Entry point of this app
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
