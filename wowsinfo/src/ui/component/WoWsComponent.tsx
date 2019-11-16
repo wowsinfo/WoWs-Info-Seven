b2f74bcfd90b8c03f9addb727cef1ab07512711e
Yiheng (NUC) <development.henryquan@gmail.com>
2019-11-16 20:54:09 +1100

    added WoWsComponent as the base as all components

---
+++ wowsinfo/src/ui/component/WoWsComponent.tsx
@@ -0,0 +1,25 @@
+import { PureComponent } from 'react';
+
+/**
+ * Props shared by all components
+ */
+export interface WoWsProps {
+
+}
+
+/**
+ * States shared by all components
+ * - Loading (whether it is loading)
+ * - error (whether it fails to load)
+ */
+export interface WoWsState {
+  loading: boolean,
+  error: boolean
+}
+
+export default abstract class WoWsComponent extends PureComponent<WoWsProps, WoWsState> {
+  /**
+   * if this is a pro feature
+   */
+  isProFeature: boolean = false;
+}
---
+++ wowsinfo/src/ui/page/Home.tsx
@@ -0,0 +1,5 @@
+class Home {
+    constructor() {
+        
+    }
+}
