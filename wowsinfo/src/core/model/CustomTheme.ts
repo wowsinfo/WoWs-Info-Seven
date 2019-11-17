8ea25732bf4991d4537ef07db7fdcabbf7246a0d
Yiheng (NUC) <development.henryquan@gmail.com>
2019-11-17 11:14:55 +1100

    Theme -> CustomTheme for theming

---
+++ wowsinfo/src/core/model/CustomTheme.ts
@@ -0,0 +1,33 @@
+import { Theme, DarkTheme, DefaultTheme } from "react-native-paper";
+
+/**
+ * Custom theme (a wrapper around Theme from react native paper)
+ * - Dark or light mode
+ * - Primary colour
+ */
+export default class CustomTheme {
+  dark!: boolean;
+  primary!: string;
+
+  constructor(dark: boolean, primary: string) {
+    this.dark = dark;
+    this.primary = primary;
+  }
+
+  /**
+   * Get your custom theme based on your selection
+   * @returns a theme object
+   */
+  getTheme(): Theme {
+    const baseTheme = this.dark ? DarkTheme : DefaultTheme;
+
+    // There might be some changes in the future
+    return {
+      ...baseTheme,
+      colors: {
+        ...baseTheme.colors,
+        primary: this.primary
+      }
+    };
+  }
+}
