efa845a0a4b8b0c0c914c56a39cf3686d7f9e499
Yiheng (NUC) <development.henryquan@gmail.com>
2020-01-27 16:58:51 +1100

    added webview for news

--- wowsinfo/src/ui/page/Home/Tabs/Account.tsx
+++ wowsinfo/src/ui/page/Home/Tabs/Account.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export interface AccountProps {
 
@@ -18,10 +19,18 @@ class Account extends Component<AccountProps, AccountState> {
   }
 
   render() {
+    const JS = "$('.footer-main, #main-top-menu, #common_menu, .mobile-socials__button, .categories-filter__header__title').remove();";
     const { container } = styles;
     return (
       <View style={container}>
-        <Text>Account</Text>
+        <WebView source={{ uri: 'https://worldofwarships.asia/en/news/' }} style={{flex: 0.5, height: '50%'}}
+          onShouldStartLoadWithRequest={(event) => {
+          console.log(event.url);
+          const { url } = event;
+          let shouldLoad = url.startsWith('https://worldofwarships') && url.includes('news');
+          return false;
+        }} injectedJavaScript={JS}
+        allowsFullscreenVideo originWhitelist={['https://wprldofwarships*']}/>
       </View>
     );
   }
