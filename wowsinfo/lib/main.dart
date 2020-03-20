0227f8ef320396f7da282a01b5627e9a5297a25b
Yiheng (NUC) <development.henryquan@gmail.com>
2020-03-20 20:43:56 +1100

    updated title

--- wowsinfo/lib/main.dart
+++ wowsinfo/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'WoWs Info Re',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
