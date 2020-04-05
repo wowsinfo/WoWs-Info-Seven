1219b0e27182e72f288c238e09234a86fe20b07a
Yiheng <development.henryquan@gmail.com>
2020-04-05 15:34:35 +1000

    rename.py is used to rename assets name

--- wowsinfo/lib/assets/rename.py
+++ wowsinfo/lib/assets/rename.py
@@ -4,5 +4,11 @@ import os, glob
 Replace string with another string, like replace xxx@2x.png with xxx.png
 '''
 
-for a in glob.glob('2.0x/'):
-    print(a)
+def replaceFileNameFromPath(path: str, findStr: str, replaceStr: str):
+    for f in glob.glob(path):
+        if findStr in f:
+            os.rename(f, f.replace(findStr, replaceStr))
+
+# replace @2x and @3x from the folder
+replaceFileNameFromPath('2.0x/*', '@2x', '')
+replaceFileNameFromPath('3.0x/*', '@3x', '')
