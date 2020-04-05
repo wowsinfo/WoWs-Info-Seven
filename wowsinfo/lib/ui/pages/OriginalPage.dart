6cdecc5b2f98cc0679849d088358aeb1a0ccda2d
Yiheng <development.henryquan@gmail.com>
2020-04-05 21:12:39 +1000

    updated assets path and add original page back

---
+++ wowsinfo/lib/ui/pages/OriginalPage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+/// This mimics the original home page back in early 2017, this is the start of everything
+class OriginalPage extends StatelessWidget {
+  OriginalPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WoWs Info'),
+        backgroundColor: Color(0XFF5A9CFF),
+      ),
+      body: Center(
+        child: Image.asset(
+          'lib/assets/images/search_button.png',
+          height: 128,
+          width: 128,
+        ),
+      ),
+    );
+  }
+}
