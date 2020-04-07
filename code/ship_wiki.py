c256bb093a0893142691ad367c55428b23d0f29b
Yiheng (NUC) <development.henryquan@gmail.com>
2020-04-07 17:57:27 +1000

    get the wiki link for all ships

--- code/ship_wiki.py
+++ code/ship_wiki.py
@@ -1,1 +1,14 @@
 import requests
+import json
+
+ship_names = {}
+# read more ship_names.json
+with open('ship_names.json', 'r', encoding='utf-8') as j:
+  ship_names = json.load(j, encoding='utf-8')
+  j.close()
+
+# get data from global wiki
+for key, value in ship_names.items():
+  name = value['name']
+  link = 'https://wiki.wargaming.net/en/Ship:' + name.replace(' ', '_')
+  print(link)
