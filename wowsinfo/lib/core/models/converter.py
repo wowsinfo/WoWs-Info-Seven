a6ae418aff09a48642815af21fea5cc709ac4813
Yiheng <development.henryquan@gmail.com>
2020-04-12 14:55:26 +1000

    converter added to add not null check

--- wowsinfo/lib/core/models/converter.py
+++ wowsinfo/lib/core/models/converter.py
@@ -1,2 +1,30 @@
+'''
+This helps to check if the object isn't null before creating any new objects
+'''
 import glob
+import re
+from os import path as p
 
+def convert(line):
+    if ("as" in line or "cast" in line or "map" in line or "=>" in line or "{" in line or not "=" in line):
+        return line
+    else:
+        m = re.search(r'\((.*)\);$', line)
+        if m == None:
+            return line
+        return '    if ({} != null) {}'.format(m.group(1), line)
+
+# example = "this.nextShip = NextShip(json['next_ships']);"
+# print(convert(example))
+
+def convertAll(path):
+    for d in glob.glob(path, recursive=True):
+        with open(d, 'r') as f:
+            # save all lines
+            lines = []
+            for line in f.readlines():
+                lines.append(convert(line))
+            with open(d, 'w') as w:
+                w.writelines(lines)
+
+convertAll('**/*.dart')
