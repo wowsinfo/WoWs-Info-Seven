4e94536ba87749466827e6e3e647c243ed94f9c4
Yiheng <development.henryquan@gmail.com>
2020-04-12 18:42:44 +1000

    added ShipInfoAdditional

--- wowsinfo/lib/core/models/User/Clan.dart
+++ wowsinfo/lib/core/models/User/Clan.dart
@@ -1,11 +1,13 @@
 import 'package:wowsinfo/core/data/Preference.dart';
 
+/// clan tag, clan id and server code
 class Clan {
   String tag;
   int clanId;
   int server;
 
   Clan(this.tag, this.clanId, this.server);
+
   Clan.fromJson(Map<String, dynamic> json) {
     this.tag = json['tag'];
     this.clanId = json['clan_id'];
