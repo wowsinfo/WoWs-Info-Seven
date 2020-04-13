51b6381d95e4dcda33d4ba0aca090377f1b442fd
Yiheng <development.henryquan@gmail.com>
2020-04-13 19:31:18 +1000

    some intense copy paste
    I should have writtern a script to do it...

--- wowsinfo/lib/core/data/CachedData.dart
+++ wowsinfo/lib/core/data/CachedData.dart
@@ -41,16 +41,74 @@ class CachedData extends LocalData {
   ///
   
   PRData _prData;
+  void loadPRData() => _prData = decode(PERSONAL_RATING, (j) => PRData.fromJson(j));
+  void savePRData(Map<String, dynamic> json) {
+    _prData = PRData.fromJson(json);
+    box.put(PERSONAL_RATING, jsonEncode(json));
+  }
+
   ShipAlias _alias;
+  void loadAlias() => _alias = decode(SHIP_ALIAS, (j) => ShipAlias.fromJson(j));
+  void saveAlias(Map<String, dynamic> json) {
+    _alias = ShipAlias.fromJson(json);
+    box.put(SHIP_ALIAS, jsonEncode(json));
+  }
 
   WikiAchievement _achievement;
+  void loadAchievement() => _achievement = decode(WIKI_ACHIEVEMENT, (j) => WikiAchievement.fromJson(j));
+  void saveAchievement(Map<String, dynamic> json) {
+    _achievement = WikiAchievement.fromJson(json);
+    box.put(WIKI_ACHIEVEMENT, jsonEncode(json));
+  }
+
   WikiWarship _warship;
+  void loadWarship() => _warship = decode(WIKI_WARSHIP, (j) => WikiWarship.fromJson(j));
+  void saveWarship(Map<String, dynamic> json) {
+    _warship = WikiWarship.fromJson(json);
+    box.put(WIKI_WARSHIP, jsonEncode(json));
+  }
+
   WikiCollection _collection;
+  void loadCollection() => _collection = decode(WIKI_COLLECTION, (j) => WikiCollection.fromJson(j));
+  void saveCollection(Map<String, dynamic> json) {
+    _collection = WikiCollection.fromJson(json);
+    box.put(WIKI_COLLECTION, jsonEncode(json));
+  }
+
   WikiCollectionItem _collectionItem;
+  void loadCollectionItem() => _collectionItem = decode(WIKI_COLLECTION_ITEM, (j) => WikiCollectionItem.fromJson(j));
+  void saveCollectionItem(Map<String, dynamic> json) {
+    _collectionItem = WikiCollectionItem.fromJson(json);
+    box.put(WIKI_COLLECTION_ITEM, jsonEncode(json));
+  }
+
   WikiCommanderSkill _commanderSkill;
+  void loadCommanderSkill() => _commanderSkill = decode(WIKI_COMMANDER_SKILL, (j) => WikiCommanderSkill.fromJson(j));
+  void saveCommanderSkill(Map<String, dynamic> json) {
+    _commanderSkill = WikiCommanderSkill.fromJson(json);
+    box.put(WIKI_COMMANDER_SKILL, jsonEncode(json));
+  }
+
   WikiGameMap _gameMap;
+  void loadGameMap() => _gameMap = decode(WIKI_GAME_MAP, (j) => WikiGameMap.fromJson(j));
+  void loadAndSavePRData(Map<String, dynamic> json) {
+    _gameMap = WikiGameMap.fromJson(json);
+    box.put(WIKI_GAME_MAP, jsonEncode(json));
+  }
+
   WikiConsumable _consumable;
+  void loadConsumable() => _consumable = decode(WIKI_CONSUMABLE, (j) => WikiConsumable.fromJson(j));
+  void saveConsumable(Map<String, dynamic> json) {
+    _consumable = WikiConsumable.fromJson(json);
+    box.put(WIKI_CONSUMABLE, jsonEncode(json));
+  }
+
   WikiEncyclopedia _encyclopedia;
+  void loadEncyclopedia() => _encyclopedia = decode(WIKI_ENCYCLOPEDIA, (j) => WikiEncyclopedia.fromJson(j));
+  void saveEncyclopedia(Map<String, dynamic> json) {
+    _encyclopedia = WikiEncyclopedia.fromJson(json);
+    box.put(WIKI_ENCYCLOPEDIA, jsonEncode(json));
+  }
 
   ///
   /// Functions
@@ -74,20 +132,24 @@ class CachedData extends LocalData {
   /// Load all cached data into memory
   /// TODO: this can be optimised like when load when necessary
   void loadAll() {
-    _prData = PRData.fromJson(decode(PERSONAL_RATING));
-    _alias = ShipAlias.fromJson(decode(SHIP_ALIAS));
-
-    _achievement = WikiAchievement.fromJson(decode(WIKI_ACHIEVEMENT));
-    _collection = WikiCollection.fromJson(decode(WIKI_COLLECTION));
-    _collectionItem = WikiCollectionItem.fromJson(decode(WIKI_COLLECTION_ITEM));
-    _commanderSkill = WikiCommanderSkill.fromJson(decode(WIKI_COMMANDER_SKILL));
-    _consumable = WikiConsumable.fromJson(decode(WIKI_CONSUMABLE));
-    _encyclopedia = WikiEncyclopedia.fromJson(decode(WIKI_ENCYCLOPEDIA));
-    _gameMap = WikiGameMap.fromJson(decode(WIKI_GAME_MAP));
-    _warship = WikiWarship.fromJson(decode(WIKI_WARSHIP));
+    loadAchievement();
+    loadAlias();
+    loadCollection();
+    loadCollectionItem();
+    loadConsumable();
+    loadEncyclopedia();
+    loadGameMap();
+    loadPRData();
+    loadWarship();
+    loadCommanderSkill();
+    
     close();
   }
 
-  /// Shorten decode process
-  Map<String, dynamic> decode(String key) => jsonDecode(box.get(key));
+  /// Shorten decode process and extra check
+  dynamic decode(String key, Function f) {
+    final saved = box.get(key);
+    if (saved == null) return null;
+    return f(jsonDecode(saved));
+  }
 }
