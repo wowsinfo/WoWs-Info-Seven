271e9ae7d56eb6f7d3dbd68f915d0c2b9821bbf3
Yiheng <development.henryquan@gmail.com>
2020-04-28 19:45:39 +1000

    updated module sorting to be stable

--- wowsinfo/lib/core/models/WoWs/WikiShipInfo.dart
+++ wowsinfo/lib/core/models/WoWs/WikiShipInfo.dart
@@ -133,11 +133,10 @@ class Part {
     // Copied from https://github.com/HenryQuan/WoWs-Info-Future/blob/legacy_version/WoWsInfo/src/page/wiki/WarshipModule.js
     if (this.priceXp != other.priceXp) {
       return this.priceXp.compareTo(other.priceXp);      
-    } else if (this.hasNextModule && other.hasNextModule) {
-      // Check if the next module of this is actually other
-      return (this.nextModule.first == other.moduleId) ? -1 : 1;
+    } else if (!this.hasNextModule && !other.hasNextModule) {
+      return this.moduleId - other.moduleId;
     } else {
-      return this.hasNextModule ? -1 : 1;
+      return this.nextModule.contains(other.moduleId) ? -1 : 1;
     }
   }
 }
