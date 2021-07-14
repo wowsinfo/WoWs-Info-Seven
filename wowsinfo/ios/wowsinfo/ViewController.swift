7729296482b77fc729debe8b4768a217e3b9d304
Yiheng <development.henryquan@gmail.com>
2021-07-14 22:33:41 +1000

    added new project and added react native module

---
+++ wowsinfo/ios/wowsinfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wowsinfo
+//
+//  Created by Yiheng Quan on 14/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
