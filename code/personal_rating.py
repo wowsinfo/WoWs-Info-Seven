121300736dd63b51433a8969b996d245d2610d4a
Yiheng <development.henryquan@gmail.com>
2021-07-16 22:00:24 +1000

    update personal rating - 2021-07-16 22:00:24.439961

--- code/personal_rating.py
+++ code/personal_rating.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import datetime
 
 link = 'https://wows-numbers.com/personal/rating/expected/json/'
 res = requests.get(link)
@@ -7,3 +9,8 @@ if res.status_code == 200:
   with open('../json/personal_rating.json', 'w+', encoding='utf-8') as f:
     f.write(res.text + '\n')
     f.close()
+
+  print("Write to personal_rating.json")
+  os.system("git commit -am 'update personal rating - {}'".format(datetime.datetime.now()))
+else:
+  exit("Status Code wasn't 200 OK")
