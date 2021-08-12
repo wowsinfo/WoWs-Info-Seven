894270f386aa220d97809678f0fbb100420c9c73
Yiheng <development.henryquan@gmail.com>
2021-08-12 21:01:30 +1000

    show git diff before commit

--- code/personal_rating.py
+++ code/personal_rating.py
@@ -6,14 +6,16 @@ input("=> Press any key to update personal rating")
 link = 'https://wows-numbers.com/personal/rating/expected/json/'
 res = requests.get(link)
 if res.status_code == 200:
-  # override
-  with open('../json/personal_rating.json', 'w+', encoding='utf-8') as f:
-    f.write(res.text + '\n')
-    f.close()
+    # override
+    with open('../json/personal_rating.json', 'w+', encoding='utf-8') as f:
+        f.write(res.text + '\n')
+        f.close()
 
-  print("=> Write to personal_rating.json")
-  os.system("git commit -am '{} update personal rating'".format(datetime.datetime.now()))
-  input("=> Press any key to push to remote")
-  os.system("git push origin API")
+    print("=> Write to personal_rating.json")
+    os.system("git diff")
+    input("=> Press any key to push to remote")
+    os.system(
+        "git commit -am '{} update personal rating'".format(datetime.datetime.now()))
+    os.system("git push origin API")
 else:
-  exit("Status Code wasn't 200 OK")
+    exit("Status Code wasn't 200 OK")
